28fe2e1502dadf40d30bce06b9d47a84022f05f3
ecc414 <ecc00414@gmail.com>
2022-09-20 12:30:52 +0900

    feat : 스토리보드 제거

--- GyroData/SceneDelegate.swift
+++ GyroData/SceneDelegate.swift
@@ -16,7 +16,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window?.windowScene = windowScene
+
+        self.window = UIWindow(windowScene: windowScene)
+
+        let rootViewController = ViewController()
+
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+
+        self.window?.rootViewController = navigationController
+
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
