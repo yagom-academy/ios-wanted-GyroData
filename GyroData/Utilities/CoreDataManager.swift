c9bd9704560c27db1a7258f7509b7c6886607be7
SChan-Kim <instufld@gmail.com>
2022-09-21 15:30:33 +0900

    feat: CoreDataManager - fetch, insert, delete, count 추가

--- GyroData/Utilities/CoreDataManager.swift
+++ GyroData/Utilities/CoreDataManager.swift
@@ -25,4 +25,59 @@ class CoreDataManager {
     var context: NSManagedObjectContext {
         return self.persistentContainer.viewContext
     }
+    
+    func fetch<T: NSManagedObject>(request: NSFetchRequest<T>) -> [T] {
+        do {
+            let fetchResult = try self.context.fetch(request)
+            return fetchResult
+        } catch {
+            // MARK: fetch 실패할 경우 에러 처리
+            print(error.localizedDescription)
+            return []
+        }
+    }
+    
+    @discardableResult
+    func insertMotionData(motion: Motion) -> Bool {
+        let entity = NSEntityDescription.entity(forEntityName: "MotionModel", in: self.context)
+        
+        if let entity = entity {
+            let managedObject = NSManagedObject(entity: entity, insertInto: self.context)
+            
+            managedObject.setValue(motion.type, forKey: "type")
+            managedObject.setValue(motion.path, forKey: "path")
+            managedObject.setValue(motion.time, forKey: "time")
+            
+            do {
+                try self.context.save()
+                return true
+            } catch {
+                // MARK: save 실패할 경우 에러 처리
+                print(error.localizedDescription)
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    
+    @discardableResult
+    func delete(object: NSManagedObject) -> Bool {
+        self.context.delete(object)
+        do {
+            try context.save()
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    func count<T: NSManagedObject>(request: NSFetchRequest<T>) -> Int? {
+        do {
+            let count = try self.context.count(for: request)
+            return count
+        } catch {
+            return nil
+        }
+    }
 }
