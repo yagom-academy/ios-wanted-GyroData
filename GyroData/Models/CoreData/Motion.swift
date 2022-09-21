ed7296d04b906429eb826d21be177b134c9a9aa1
SChan-Kim <instufld@gmail.com>
2022-09-21 15:09:55 +0900

    feat: CoreData 관련 Motion model 생성

---
+++ GyroData/Models/CoreData/Motion.swift
@@ -0,0 +1,14 @@
+//
+//  Motion.swift
+//  GyroData
+//
+//  Created by channy on 2022/09/21.
+//
+
+import Foundation
+
+struct Motion {
+    var type: String
+    var path: String
+    var time: Date
+}
