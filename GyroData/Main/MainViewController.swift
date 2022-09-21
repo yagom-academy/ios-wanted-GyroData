6a7dd7a890f7e439666562856f9c50f79481ca15
julia0926 <kj980926@naver.com>
2022-09-21 11:26:53 +0900

    storyboard 제거 및 폴더 정리

--- GyroData/Main/MainViewController.swift
+++ GyroData/Main/MainViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red //Test 코드입니다 ! 지우셔도 되요
     }
 
 
---
+++ GyroData/Measure/MeasureViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MeasureViewController.swift
+//  GyroData
+//
+//  Created by 김지인 on 2022/09/21.
+//
+
+import UIKit
+
+class MeasureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
