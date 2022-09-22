e1c0da5eed953062f085eeecfceb3ad4c6cd2325
SChan-Kim <instufld@gmail.com>
2022-09-22 17:43:58 +0900

    test: FileManager 를 통한 입/출력

--- GyroData/Utilities/FileManager.swift
+++ GyroData/Utilities/FileManager.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+extension FileManager {
+
+    func createFileDirectory() {
+        let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let directoryURL = documentURL.appendingPathExtension("MotionData")
+        
+        do {
+            try self.createDirectory(at: directoryURL, withIntermediateDirectories: false, attributes: nil)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func saveFileToFileDirectory(file: MotionFile) {
+        let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let directoryURL = documentURL.appendingPathExtension("MotionData")
+        let fileURL = directoryURL.appendingPathExtension(file.fileName)
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let encodedData = try jsonEncoder.encode(file)
+            
+            do {
+                print(String(data: encodedData, encoding: .utf8)!)
+                try encodedData.write(to: fileURL)
+            } catch let e as NSError {
+                print(e.localizedDescription)
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loadJsonFile(name: String) -> MotionFile? {
+        let jsonDecoder = JSONDecoder()
+        let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let directoryURL = documentURL.appendingPathExtension("MotionData3")
+        let fileURL = directoryURL.appendingPathExtension(name)
+        
+        do {
+            let jsonData = try Data(contentsOf: fileURL, options: .mappedIfSafe)
+            let decodeJson = try jsonDecoder.decode(MotionFile.self, from: jsonData)
+            return decodeJson
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+}
