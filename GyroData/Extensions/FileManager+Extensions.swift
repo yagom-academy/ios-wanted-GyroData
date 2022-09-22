a1644a9d75bdb64ab91987ab2b8ef755b83bae1b
SChan-Kim <instufld@gmail.com>
2022-09-22 23:17:24 +0900

    refactor: FileManager+Extensions 경로 변경

--- GyroData/Extensions/FileManager+Extensions.swift
+++ GyroData/Extensions/FileManager+Extensions.swift
@@ -9,24 +9,13 @@ import Foundation
 
 extension FileManager {
 
-    func createFileDirectory() {
-        let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let directoryURL = documentURL.appendingPathExtension("MotionData")
-        
-        do {
-            try self.createDirectory(at: directoryURL, withIntermediateDirectories: false, attributes: nil)
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-    
-    func saveFileToFileDirectory(file: MotionFile) {
+    func saveMotionFile(file: MotionFile) {
         let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let directoryURL = documentURL.appendingPathExtension("MotionData")
         let fileURL = directoryURL.appendingPathExtension(file.fileName)
-        let jsonEncoder = JSONEncoder()
         
         do {
+            let jsonEncoder = JSONEncoder()
             let encodedData = try jsonEncoder.encode(file)
             
             do {
@@ -35,18 +24,19 @@ extension FileManager {
             } catch let e as NSError {
                 print(e.localizedDescription)
             }
+            
         } catch let error {
             print(error.localizedDescription)
         }
     }
     
-    func loadJsonFile(name: String) -> MotionFile? {
-        let jsonDecoder = JSONDecoder()
+    func loadMotionFile(name: String) -> MotionFile? {
         let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let directoryURL = documentURL.appendingPathExtension("MotionData3")
         let fileURL = directoryURL.appendingPathExtension(name)
         
         do {
+            let jsonDecoder = JSONDecoder()
             let jsonData = try Data(contentsOf: fileURL, options: .mappedIfSafe)
             let decodeJson = try jsonDecoder.decode(MotionFile.self, from: jsonData)
             return decodeJson
