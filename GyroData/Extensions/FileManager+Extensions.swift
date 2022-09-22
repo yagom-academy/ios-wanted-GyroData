beacf6211b1cb802e6bcba92b3734030b347c564
SChan-Kim <instufld@gmail.com>
2022-09-23 00:16:39 +0900

    fix: json 파일 저장 directory

--- GyroData/Extensions/FileManager+Extensions.swift
+++ GyroData/Extensions/FileManager+Extensions.swift
@@ -8,11 +8,26 @@
 import Foundation
 
 extension FileManager {
+    
+    // 공통 Path 는 Repository 에서 관리해야할까?
+    // Directory 가 없을 때 만들어줘야 한다 -> AppDelegate?
+    
+    func createDirectory() {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let directoryURL = documentsURL.appendingPathComponent("MotionData")
+        
+        do {
+            try FileManager.default.createDirectory(at: directoryURL, withIntermediateDirectories: false, attributes: nil)
+        } catch let e {
+            print(e.localizedDescription)
+        }
+    }
 
     func saveMotionFile(file: MotionFile) {
+        // TO-DO : Refactor - directory
         let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let directoryURL = documentURL.appendingPathExtension("MotionData")
-        let fileURL = directoryURL.appendingPathExtension(file.fileName)
+        let directoryURL = documentURL.appendingPathComponent("MotionData")
+        let fileURL = directoryURL.appendingPathComponent("\(file.fileName).json")
         
         do {
             let jsonEncoder = JSONEncoder()
@@ -31,9 +46,10 @@ extension FileManager {
     }
     
     func loadMotionFile(name: String) -> MotionFile? {
+        // TO-DO : Refactor - directory
         let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let directoryURL = documentURL.appendingPathExtension("MotionData3")
-        let fileURL = directoryURL.appendingPathExtension(name)
+        let directoryURL = documentURL.appendingPathComponent("MotionData")
+        let fileURL = directoryURL.appendingPathComponent("\(name).json")
         
         do {
             let jsonDecoder = JSONDecoder()
@@ -44,5 +60,25 @@ extension FileManager {
             print(error)
             return nil
         }
+    }
+    
+    @discardableResult
+    func removeMotionFile(name: String) -> Error? {
+        // TO-DO : Refactor - directory
+        let documentURL = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let directoryURL = documentURL.appendingPathComponent("MotionData")
+        let fileURL = directoryURL.appendingPathComponent("\(name).json")
+        
+        guard !FileManager.default.fileExists(atPath: "\(fileURL)") else {
+            return NSError(domain: "File does not exist", code: -1) as Error
+        }
+        
+        do {
+            try FileManager.default.removeItem(at: fileURL)
+        } catch let error {
+            return error
+        }
+        
+        return nil
     }
 }
