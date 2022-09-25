8e8f9a3919b8a4539b762a631a58a9a7ec0e1dc7
SChan-Kim <instufld@gmail.com>
2022-09-26 01:36:42 +0900

    test: MotionFile Generator

--- GyroData/Utilities/DummyGenerator.swift
+++ GyroData/Utilities/DummyGenerator.swift
@@ -15,4 +15,23 @@ class DummyGenerator {
         let path = ""
         return MotionTask(type: type, time: time, date: date, path: path)
     }
+    
+    static func getDummyMotionFile() -> MotionFile {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd-HH:mm:ss"
+        
+        let fileName = dateFormatter.string(from: Date(timeIntervalSinceNow: Double.random(in: 0...50000)))
+        let type = Int.random(in: 0...1) % 2 == 0 ? "GYRO" : "ACC"
+        var x_axis: [Float] = []
+        var y_axis: [Float] = []
+        var z_axis: [Float] = []
+        
+        for _  in 0..<10 {
+            x_axis.append(Float.random(in: 0...1))
+            y_axis.append(Float.random(in: 0...1))
+            z_axis.append(Float.random(in: 0...1))
+        }
+        
+        return MotionFile(fileName: fileName, type: type, x_axis: x_axis, y_axis: y_axis, z_axis: z_axis)
+    }
 }
