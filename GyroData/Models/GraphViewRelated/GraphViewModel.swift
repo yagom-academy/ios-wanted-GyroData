c979bac69dcb8403f08ce668352ee9027d52e8d0
JunYoungJee <rainmaker8492@gmail.com>
2022-09-27 00:27:05 +0900

    측정 버튼 클릭시 SecondModel -> GraphViewModel로 데이터 전달
    들어온 데이터에 대한 수치값 보정 필요

--- GyroData/Models/GraphViewRelated/GraphViewModel.swift
+++ GyroData/Models/GraphViewRelated/GraphViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 class GraphViewModel {
     
     //input
-    var didReceiveData: (ValueInfo) -> () = { value in }
+    var didReceiveData: ([MotionMeasure]) -> () = { measure in }
     var didReceiveRemoveAll = { }
     
     //output
@@ -29,10 +29,10 @@ class GraphViewModel {
     }
     
     func bind() {
-        didReceiveData = { [weak self] value in
-            guard let calculatedValue = self?.calculateValue(value: value) else { return }
+        didReceiveData = { [weak self] measure in
+            guard let calculatedValue = self?.calculateValue(value: measure) else { return }
             
-            self?.privateDataSource.append(calculatedValue)
+            self?.privateDataSource = calculatedValue
             self?.populateData()
         }
         
@@ -45,13 +45,15 @@ class GraphViewModel {
     //Gyro, acc 값의 최소, 최대값은 0.0 ~ 1.0 으로 추정
     //그러므로 ViewModel에서 "그래프가 표시할 수 있는 정도의 값"으로 보정을 해야함
     //TODO: 실제 데이터와 연결하면서 더 나은 데이터 범위로 나오도록 보정
-    private func calculateValue(value: ValueInfo) -> ValueInfo {
-        //0.0~100.0
-        var newValue = ValueInfo(xValue: 0, yValue: 0, zValue: 0)
-        newValue.xValue = value.xValue * 50.0 * 2
-        newValue.yValue = value.yValue * 50.0 * 2
-        newValue.zValue = value.zValue * 50.0 * 2
-        
+    private func calculateValue(value: [MotionMeasure]) -> [ValueInfo] {
+        var newValue: [ValueInfo] = value.map { measure in
+            //0.0~100.0
+            var valueInfo = ValueInfo(xValue: 0, yValue: 0, zValue: 0)
+            valueInfo.xValue = measure.x * 50.0 * 2
+            valueInfo.yValue = measure.y * 50.0 * 2
+            valueInfo.zValue = measure.z * 50.0 * 2
+            return valueInfo
+        }
         return newValue
     }
 }
