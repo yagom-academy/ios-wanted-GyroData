94db8afcd4f79b9b32fc3894ba8b3a8c5c7dbc8b
SChan-Kim <instufld@gmail.com>
2022-09-26 10:58:55 +0900

    fix: resolve conflicts

--- GyroData/Models/SecondVCRelated/SecondSegmentViewModel.swift
+++ GyroData/Models/SecondVCRelated/SecondSegmentViewModel.swift
@@ -9,15 +9,13 @@ import Foundation
 
 class SecondSegmentViewModel {
     // MARK: Input
-    var didSegmentChange: (String) -> () = { type in }
+    var didSegmentChange: (MotionType) -> () = { type in }
+    var didReceiveIsMeasuring: (Bool) -> () = { isMeasuring in }
     
     // MARK: Output
     // 이 클로저는 View Binding에만 사용하는 중입니다.
     // SecondVC에서 "저장" 버튼을 눌렀을 때 SecondModel에서 CoreData와 FileManager 저장을 진행하는데,
     // SecondVC가 selectedType의 값을 어떻게 가져갈 지 고민중입니다. Rx..?
-    var selectedTypeSource: (String) -> () = { type in } {
-    var didSegmentChange: (MotionType) -> () = { type in }
-    var didReceiveIsMeasuring: (Bool) -> () = { isMeasuring in }
     var selectedTypeSource: (MotionType) -> () = { type in } {
         didSet {
             selectedTypeSource(_selectedType)
@@ -40,9 +38,6 @@ class SecondSegmentViewModel {
             isUserInteractionEnabledSource(!_isMeasuring)
         }
     }
-    
-    // MARK: Properties
-    private var _selectedType: String
     
     init() {
 //        _selectedType = "ACC"
