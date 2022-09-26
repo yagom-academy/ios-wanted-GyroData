a2ef86882f33a8adea14bc25627b54ea830c38c1
JunYoungJee <rainmaker8492@gmail.com>
2022-09-26 17:41:36 +0900

    0.0~1.0 사이로 들어올 자이로, acc에 대한 임의의 보정 로직 추가
    실제 데이터 들어오는 것 확인하면서 더 적절한 값으로 보정 필요

--- GyroData/Models/GraphViewRelated/GraphViewModel.swift
+++ GyroData/Models/GraphViewRelated/GraphViewModel.swift
@@ -30,7 +30,8 @@ class GraphViewModel {
     
     func bind() {
         didReceiveData = { [weak self] value in
-            let calculatedValue = self?.calculateValue(value: value)
+            guard let calculatedValue = self?.calculateValue(value: value) else { return }
+            
             self?.privateDataSource.append(calculatedValue)
             self?.populateData()
         }
@@ -44,9 +45,14 @@ class GraphViewModel {
     //Gyro, acc 값의 최소, 최대값은 0.0 ~ 1.0 으로 추정
     //그러므로 ViewModel에서 "그래프가 표시할 수 있는 정도의 값"으로 보정을 해야함
     //TODO: 실제 데이터와 연결하면서 더 나은 데이터 범위로 나오도록 보정
-    private func calculateValue(value: CGFloat) -> CGFloat {
-        var calculation: CGFloat = value * 50.0 * 2 //0.0~100.0
-        return calculation
+    private func calculateValue(value: ValueInfo) -> ValueInfo {
+        //0.0~100.0
+        var newValue = ValueInfo(xValue: 0, yValue: 0, zValue: 0)
+        newValue.xValue = value.xValue * 50.0 * 2
+        newValue.yValue = value.yValue * 50.0 * 2
+        newValue.zValue = value.zValue * 50.0 * 2
+        
+        return newValue
     }
 }
 
