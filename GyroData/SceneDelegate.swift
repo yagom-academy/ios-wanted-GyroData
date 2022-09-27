3e8098232400582a11cdd8fca6d03e2cf9730683
강민교 <98341623+KangMinGyo@users.noreply.github.com>
2022-09-27 21:47:01 +0900

    Update SceneDelegate.swift
    코드 수정

--- GyroData/SceneDelegate.swift
+++ GyroData/SceneDelegate.swift
@@ -17,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
+        
         let navigationController = UINavigationController(rootViewController: ViewController())
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible() //네비게이션 추가
