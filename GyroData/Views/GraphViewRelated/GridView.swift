8a96d1733559e918eae24e18ec8f3c8657e18677
JunYoungJee <rainmaker8492@gmail.com>
2022-09-28 00:16:26 +0900

    draw에서 선 그리기 호출 + 선 그리기 반복적으로 호출되는 상황 막기 위해 플래그 추가

--- GyroData/Views/GraphViewRelated/GridView.swift
+++ GyroData/Views/GraphViewRelated/GridView.swift
@@ -9,25 +9,23 @@ import UIKit
 import SwiftUI
 
 class GridView: UIView {
-    //칸 하나의 사이즈는 대략 10 x 10?
-    //for로 가로선 쭉쭉 만들고
-    //for로 세로선 쭉쭉 만들고
-    lazy var width = self.frame.width
-    lazy var height = self.frame.height
+
     let fixedWidth: CGFloat = 30.0
     let fixedHeight: CGFloat = 30.0
     
     var fixedWidthChecker: CGFloat = 0.0
     var fixedHeightChecker: CGFloat = 0.0
     
+    var didDrawGridLines = false
+    
     override func draw(_ rect: CGRect) {
-        print("draw called : \(rect.height) \(rect.width)")
-        test(rect: rect)
+        if didDrawGridLines == false {
+            drawGridLines(rect: rect)
+        }
     }
     
     init() {
         super.init(frame: .zero)
-//        initViewHierarchy()
         configureView()
         bind()
     }
@@ -40,16 +38,13 @@ class GridView: UIView {
 
 extension GridView: Presentable {
     
-    func test(rect: CGRect) {
-        
+    func drawGridLines(rect: CGRect) {
+        didDrawGridLines = true
         var constraint: [NSLayoutConstraint] = []
         defer { NSLayoutConstraint.activate(constraint) }
         
-        width = rect.width
-        height = rect.height
-        
         //frame을 너무 일찍 가져와서 문제
-        while width > fixedWidthChecker {
+        while rect.width > fixedWidthChecker {
             fixedWidthChecker += fixedWidth
             let verticalLine = LineView()
             verticalLine.translatesAutoresizingMaskIntoConstraints = false
@@ -65,7 +60,7 @@ extension GridView: Presentable {
         }
         
         //frame을 너무 일찍 가져와서 문제
-        while height > fixedHeightChecker {
+        while rect.height > fixedHeightChecker {
             fixedHeightChecker += fixedHeight
             let horizontalLine = LineView()
             horizontalLine.translatesAutoresizingMaskIntoConstraints = false
@@ -83,47 +78,10 @@ extension GridView: Presentable {
     
     func initViewHierarchy() {
         
-        print("initViewHierachy")
-        var constraint: [NSLayoutConstraint] = []
-        defer { NSLayoutConstraint.activate(constraint) }
-        
-        print("width check heicht check : \(width) \(height)")
-        //frame을 너무 일찍 가져와서 문제
-        while 360 > fixedWidthChecker {
-            fixedWidthChecker += fixedWidth
-            let verticalLine = LineView()
-            verticalLine.translatesAutoresizingMaskIntoConstraints = false
-            self.addSubview(verticalLine)
-            
-            constraint += [
-                verticalLine.widthAnchor.constraint(equalToConstant: 1.0),
-                verticalLine.topAnchor.constraint(equalTo: self.topAnchor),
-                verticalLine.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-                verticalLine.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: fixedWidthChecker)
-            ]
-            print("width called")
-        }
-        
-        //frame을 너무 일찍 가져와서 문제
-        while 720 > fixedHeightChecker {
-            fixedHeightChecker += fixedHeight
-            let horizontalLine = LineView()
-            horizontalLine.translatesAutoresizingMaskIntoConstraints = false
-            self.addSubview(horizontalLine)
-            
-            constraint += [
-                horizontalLine.heightAnchor.constraint(equalToConstant: 1.0),
-                horizontalLine.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-                horizontalLine.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-                horizontalLine.topAnchor.constraint(equalTo: self.topAnchor, constant: fixedHeightChecker)
-            ]
-            print("height called")
-        }
-        
     }
     
     func configureView() {
-        self.backgroundColor = .red
+        self.backgroundColor = .white
     }
     
     func bind() {
