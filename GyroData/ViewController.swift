542a15f17e5723e262ebdc6922520ba0db36748c
wongbingg <wb277@naver.com>
2022-12-26 14:12:37 +0900

    style : 코드베이스 UI 작성을 위한 초기설정

--- GyroData/ViewController.swift
+++ GyroData/ViewController.swift
@@ -11,9 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupInitialView()
     }
-
-
+    
+    private func setupInitialView() {
+        view.backgroundColor = .systemBackground
+    }
 }
-
