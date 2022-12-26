c5ad2aeb8d0674a9d89a10c490ac243d72091354
Ari <lee_ari95@icloud.com>
2022-12-26 20:16:21 +0900

    feat: MotionEntity를 관리하는 Storage 타입 구현

---
+++ GyroData/Sources/Utility/CoreDataStorage/MotionStorage/CoreDataMotionStorage.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataMotionStorage.swift
+//  GyroData
+//
+//  Created by Ari on 2022/12/26.
+//
+
+import Foundation
+
+final class CoreDataMotionStorage {
+    
+    private let coreDataStorage: CoreDataStorage
+    
+    init(coreDataStorage: CoreDataStorage = CoreDataStorage.shared) {
+        self.coreDataStorage = coreDataStorage
+    }
+    
+}
+
+extension CoreDataMotionStorage: MotionStorage {
+    
+    func fetch() -> [MotionEntity] {
+        let context = coreDataStorage.persistentContainer.viewContext
+        let object = try? coreDataStorage.getMotion(context)
+        return object ?? []
+    }
+    
+    func insert(_ motion: Motion) {
+        coreDataStorage.performBackgroundTask { context in
+            _ = MotionEntity(motion: motion, context: context)
+            context.saveContext()
+        }
+    }
+    
+    func delete(_ item: MotionEntity) {
+        coreDataStorage.performBackgroundTask { context in
+            context.delete(item)
+            context.saveContext()
+        }
+    }
+}
