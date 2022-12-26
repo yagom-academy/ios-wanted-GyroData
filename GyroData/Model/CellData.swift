7095b3b588da3346ea309e9b7d42e04f18e00bed
wongbingg <wb277@naver.com>
2022-12-26 16:00:16 +0900

    feat : CellData 생성

---
+++ GyroData/Model/CellData.swift
@@ -0,0 +1,14 @@
+//
+//  CellData.swift
+//  GyroData
+//
+//  Created by 이원빈 on 2022/12/26.
+//
+
+import Foundation
+
+struct CellData {
+    let date: String
+    let sensorValue: String
+    let sensor: String
+}
