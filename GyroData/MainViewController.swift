04c1e14d7b729a364d6da0a22c5d2ccbdd35cdca
BaekGom <jihunmm02@gmail.com>
2022-12-26 16:11:11 +0900

    feat: MainTableView 구현
    - Layout 설정

--- GyroData/MainViewController.swift
+++ GyroData/MainViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    private let listTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(MainTableViewCell.self , forCellReuseIdentifier: MainTableViewCell.identifier)
+        tableView.backgroundColor = .red
+        return tableView
+    }()
 
     // MARK: - View Life Cycle
     
@@ -21,6 +28,8 @@ final class MainViewController: UIViewController {
     
     private func commonInit() {
         setupNavigationBar()
+        setupSubView()
+        setupConstraint()
     }
     
     private func setupNavigationBar() {
@@ -46,4 +55,24 @@ final class MainViewController: UIViewController {
     @objc private func rightBarButtonTapped() {
         print("view move")
     }
+    
+    private func setupSubView() {
+        view.addSubview(listTableView)
+//        listTableView.dataSource = self
+//        listTableView.delegate = self
+    }
+    
+    private func setupConstraint() {
+        setupTableViewConstraint()
+    }
+    
+    private func setupTableViewConstraint() {
+        NSLayoutConstraint.activate([
+            listTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            listTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            listTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            listTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
+
