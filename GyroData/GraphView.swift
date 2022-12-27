ff3de48b216233da48ee383a933474e86e64dc4d
aCafela-coffee <acafelacoffee@gmail.com>
2022-12-27 13:00:06 +0900

    [Feat] #1 그래프의 x, y, z현재값 표시하는 로직 제작

--- GyroData/GraphView.swift
+++ GyroData/GraphView.swift
@@ -8,6 +8,27 @@
 import UIKit
 
 final class GraphView: UIView {
+    private let redLabel: UILabel = {
+        let label = UILabel()
+        label.text = "x:0"
+        label.textColor = UIColor.red
+        return label
+    }()
+
+    private let blueLabel: UILabel = {
+        let label = UILabel()
+        label.text = "y:0"
+        label.textColor = UIColor.blue
+        return label
+    }()
+
+    private let greenLabel: UILabel = {
+        let label = UILabel()
+        label.text = "z:0"
+        label.textColor = UIColor.green
+        return label
+    }()
+
     private var viewModel = GraphViewModel()
 
     private var widthFor1Hz: CGFloat {
@@ -21,6 +42,16 @@ final class GraphView: UIView {
     private lazy var redLinesLayer = initializeLayer(color: UIColor.red.cgColor)
     private lazy var blueLinesLayer = initializeLayer(color: UIColor.blue.cgColor)
     private lazy var greenLinesLayer = initializeLayer(color: UIColor.green.cgColor)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func draw(_ rect: CGRect) {
         let backgroundLayer = initializeBackgroundLayer()
@@ -36,16 +67,34 @@ final class GraphView: UIView {
         switch layerType {
         case .red:
             layer = redLinesLayer
+            redLabel.text = "x:\(value)"
         case .blue:
             layer = blueLinesLayer
+            blueLabel.text = "y:\(value)"
         case .green:
             layer = greenLinesLayer
+            greenLabel.text = "z:\(value)"
         }
 
         guard let path = layer?.path?.mutableCopy() else { return }
         let nextPoint = CGPoint(x: path.currentPoint.x + widthFor1Hz, y: center.y - value * heightFor1Hz)
         path.addLine(to: nextPoint)
         layer?.path = path
+    }
+
+    private func layout() {
+        [redLabel, blueLabel, greenLabel].forEach {
+            self.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        NSLayoutConstraint.activate([
+            redLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            blueLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            greenLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            redLabel.topAnchor.constraint(equalTo: topAnchor),
+            blueLabel.topAnchor.constraint(equalTo: topAnchor),
+            greenLabel.topAnchor.constraint(equalTo: topAnchor)
+        ])
     }
 
     private func drawPath(from start: CGPoint, to end: CGPoint) -> CGPath {
