45c53274c9e6e80e70949d058c1a3074c3caf853
Neph <tony3322@naver.com>
2022-12-27 20:10:44 +0900

    [Feat] #5 불필요한 Foundation import 제거

--- GyroData/Coordinate.swift
+++ GyroData/Coordinate.swift
@@ -5,8 +5,6 @@
 //  Created by 천수현 on 2022/12/27.
 //
 
-import Foundation
-
 struct Coordiante {
     let x: Double
     let y: Double
--- GyroData/MotionMode.swift
+++ GyroData/MotionMode.swift
@@ -5,8 +5,6 @@
 //  Created by 천수현 on 2022/12/27.
 //
 
-import Foundation
-
 enum MotionMode {
     case accelerometer
     case gyroscope
