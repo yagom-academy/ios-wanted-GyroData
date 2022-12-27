f27daede247fb2008b34021b1486dc055d24b876
Hugh-github <tnghk4822@gmail.com>
2022-12-27 12:58:50 +0900

    feat: 첫 번째 화면에 사용할 Cell 구현
    - 화면 확인을 위해 ViewController에 코드 작성

--- GyroData/Source/Controller/ViewController.swift
+++ GyroData/Source/Controller/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(FirstTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        configureLayout()
     }
-
-
+    
+    private func configureLayout() {
+        self.view.backgroundColor = .white
+        self.view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? FirstTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.setText(date: "2022.12.27 12:00:00", type: "Acc", time: "60.0")
+        return cell
+    }
+}
