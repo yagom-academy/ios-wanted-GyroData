43b7ba75334abea28fd9dc4581580acde4af876d
unchain123 <unchain123@kakao.com>
2022-12-27 21:09:15 +0900

    feat: SwiftUI Charts를 이용하여 GraphView 구현

---
+++ GyroData/Domain/GraphView.swift
@@ -0,0 +1,78 @@
+//
+//  GraphView.swift
+//  GyroData
+//
+//  Created by unchain on 2022/12/27.
+//
+
+import Charts
+import SwiftUI
+
+let dummyData: [Analysis] = [
+    .init(analysisType: .accelerate, x: 1, y: 2, z: 3, measurementTime: Date(timeIntervalSince1970: 0), savedAt: Date()),
+    .init(analysisType: .accelerate, x: 2, y: 3, z: 3, measurementTime: Date(timeIntervalSince1970: 10), savedAt: Date()),
+    .init(analysisType: .accelerate, x: 3, y: 4, z: 3, measurementTime: Date(timeIntervalSince1970: 20), savedAt: Date()),
+    .init(analysisType: .accelerate, x: 4, y: 5, z: 3, measurementTime: Date(timeIntervalSince1970: 30), savedAt: Date()),
+    .init(analysisType: .accelerate, x: 5, y: 6, z: 3, measurementTime: Date(timeIntervalSince1970: 40), savedAt: Date()),
+    .init(analysisType: .accelerate, x: 6, y: 7, z: 3, measurementTime: Date(timeIntervalSince1970: 50), savedAt: Date())
+]
+
+struct GraphView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            FigureView()
+            Chart() {
+                ForEach(dummyData, id: \.measurementTime) { data in
+                    LineMark(
+                        x: .value("Time", data.measurementTime),
+                        y: .value("value", data.x),
+                        series: .value("value", "x")
+                    )
+                    .foregroundStyle(.red)
+                }
+                ForEach(dummyData, id: \.measurementTime) { data in
+                    LineMark(
+                        x: .value("Time", data.measurementTime),
+                        y: .value("value", data.y),
+                        series: .value("value", "y")
+                    )
+                    .foregroundStyle(.green)
+                }
+                ForEach(dummyData, id: \.measurementTime) { data in
+                    LineMark(
+                        x: .value("Time", data.measurementTime),
+                        y: .value("value", data.z),
+                        series: .value("value", "z")
+                    )
+                    .foregroundStyle(.blue)
+                }
+            }
+            .padding()
+        }
+    }
+
+    struct FigureView: View {
+        var body: some View {
+            HStack {
+                Text("xdata")
+                    .font(.caption2)
+                    .foregroundColor(.red)
+                Spacer()
+                Text("ydata")
+                    .font(.caption2)
+                    .foregroundColor(.green)
+                Spacer()
+                Text("zdata")
+                    .font(.caption2)
+                    .foregroundColor(.blue)
+            }
+            .padding([.top, .leading, .trailing], 30)
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphView()
+    }
+}
