ee8846089c4da89214fbc39d2480ef9dca083e73
llghdud921 <llghdud921@naver.com>
2022-12-27 20:40:39 +0900

    feat: Motion 타입 생성 #1

---
+++ GyroData/Model/Motion.swift
@@ -0,0 +1,20 @@
+//
+//  Motion.swift
+//  GyroData
+//
+//  Created by 이호영 on 2022/12/27.
+//
+
+import Foundation
+
+struct Motion {
+    let id: UUID
+    let motionType: MotionType
+    let date: Date
+    let time: Double
+}
+
+enum MotionType: String {
+    case acc = "Accelerometer"
+    case gyro = "Gyro"
+}
