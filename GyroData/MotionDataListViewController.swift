f1845bcf6f3f4569729fa23edadb5f2e397cda2c
Neph <tony3322@naver.com>
2022-12-27 21:25:28 +0900

    [Feat] #3 recordTableView 제작, navigationBar set up

---
+++ GyroData/MotionDataListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MotionDataListViewController.swift
+//  GyroData
+//
+//  Created by 천수현 on 2022/12/27.
+//
+
+import UIKit
+
+final class MotionDataListViewController: UIViewController {
+    private lazy var recordTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+        setUpNavigationBar()
+    }
+
+    private func layout() {
+        view.addSubview(recordTableView)
+        NSLayoutConstraint.activate([
+            recordTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            recordTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            recordTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            recordTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    private func setUpNavigationBar() {
+        navigationItem.title = "목록"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "측정", style: .plain,
+                                                            target: self,action: #selector(measureButtonTapped(_:)))
+    }
+
+    @objc
+    private func measureButtonTapped(_ sender: UIButton) {
+
+    }
+}
+
+extension MotionDataListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
