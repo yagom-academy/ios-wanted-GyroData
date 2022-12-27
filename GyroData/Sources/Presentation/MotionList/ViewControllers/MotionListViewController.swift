ba16faee699aa07572b3da909f7f9eea23f58173
Ari <lee_ari95@icloud.com>
2022-12-27 11:46:55 +0900

    ✨ [feat] MotionList 화면에 TableView를 추가

--- GyroData/Sources/Presentation/MotionList/ViewControllers/MotionListViewController.swift
+++ GyroData/Sources/Presentation/MotionList/ViewControllers/MotionListViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 class MotionListViewController: UIViewController {
 
     weak var coordinator: MotionListCoordinatorInterface?
-        
     private let viewModel: MotionListViewModel
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 90
+        tableView.backgroundColor = .white
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpNavigationBar()
-        
+        setUp()
     }
 
     init(viewModel: MotionListViewModel, coordinator: MotionListCoordinatorInterface) {
@@ -28,6 +33,11 @@ class MotionListViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp() {
+        setUpNavigationBar()
+        setUpView()
     }
     
     private func setUpNavigationBar() {
@@ -42,5 +52,15 @@ class MotionListViewController: UIViewController {
     
     @objc private func didTapMeasure(_ sender: UIBarButtonItem) {
         coordinator?.showMotionMeasureView()
+    }
+    
+    private func setUpView() {
+        view.addSubviews(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 }
