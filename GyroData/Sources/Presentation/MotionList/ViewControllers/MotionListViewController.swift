58249a6bbac00ad8d8b6a34f6ea7cdae551804c8
Ari <lee_ari95@icloud.com>
2022-12-27 15:20:42 +0900

    ✨ [feat] MotionListViewController 내부에 뷰모델 바인딩 처리

--- GyroData/Sources/Presentation/MotionList/ViewControllers/MotionListViewController.swift
+++ GyroData/Sources/Presentation/MotionList/ViewControllers/MotionListViewController.swift
@@ -15,7 +15,7 @@ class MotionListViewController: UIViewController {
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.rowHeight = 90
-        tableView.backgroundColor = .white
+        tableView.backgroundColor = .systemBackground
         tableView.register(MotionDataCell.self)
         tableView.dataSource = self
         tableView.delegate = self
@@ -25,6 +25,7 @@ class MotionListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUp()
+        bind()
     }
 
     init(viewModel: MotionListViewModel, coordinator: MotionListCoordinatorInterface) {
@@ -36,6 +37,12 @@ class MotionListViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bind() {
+        viewModel.motions.observe(on: self) { [weak self] _ in
+            self?.tableView.reloadData()
+        }
     }
     
     private func setUp() {
@@ -96,11 +103,12 @@ extension MotionListViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(MotionDataCell.self, for: indexPath) else {
             return UITableViewCell()
         }
+        cell.setUp(by: viewModel.motions.value[safe: indexPath.row])
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return viewModel.motions.value.count
     }
     
 }
