77c611ce34a7b0e4a02b8d7b3a14905362519d98
Ari <lee_ari95@icloud.com>
2022-12-27 11:29:27 +0900

    ✨ [feat] Navigation Bar 설정 구현

--- GyroData/Sources/Presentation/MotionList/ViewControllers/MotionListViewController.swift
+++ GyroData/Sources/Presentation/MotionList/ViewControllers/MotionListViewController.swift
@@ -15,6 +15,7 @@ class MotionListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpNavigationBar()
         
     }
 
@@ -23,11 +24,23 @@ class MotionListViewController: UIViewController {
         self.coordinator = coordinator
         super.init(nibName: nil, bundle: nil)
         view.backgroundColor = .systemBackground
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setUpNavigationBar() {
+        navigationItem.title = "목록"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "측정",
+            style: .plain,
+            target: self,
+            action: #selector(didTapMeasure(_:))
+        )
+    }
+    
+    @objc private func didTapMeasure(_ sender: UIBarButtonItem) {
+        print(#function)
+    }
 }
