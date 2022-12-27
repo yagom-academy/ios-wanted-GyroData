15e5e05be1f813f830fd734fb1d0a7394c7c52fe
Neph <tony3322@naver.com>
2022-12-27 19:50:14 +0900

    [Feat] #5 Coordinate 구조체 제작

---
+++ GyroData/Coordinate.swift
@@ -0,0 +1,14 @@
+//
+//  MotionRecord.swift
+//  GyroData
+//
+//  Created by 천수현 on 2022/12/27.
+//
+
+import Foundation
+
+struct Coordiante {
+    let x: Double
+    let y: Double
+    let z: Double
+}
