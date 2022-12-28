dad7ac68b9fb080e067129ad68828a9fd3c32ec7
minsson <mskaang@gmail.com>
2022-12-28 19:40:00 +0900

    feat : GraphView의 배경이 될 GraphBackgroundView 구현
    - 추후 선의 개수나 간격 등에 대한 요구사항 변경에 대한 대응이 쉽도록 구현함
        - 각 속성의 네임스페이스 구현
            - 선의 개수, 색, 두께
        - 선을 그리는 메서드들의 분리
            - 가로, 세로선 개수가 달라질 수 있음

--- GyroData/ChartView/GraphView.swift
+++ GyroData/ChartView/GraphView.swift
@@ -35,12 +35,23 @@ final class GraphView: UIView, TickReceivable {
 final class GraphContainerView: UIView {
     private enum Configuration {
         static let borderWidth: CGFloat = 4
+        static let edgeDistance: CGFloat = 10
     }
+    
+    private let chartBackgroundView: GraphBackgroundView = {
+        let chartBackgroundView = GraphBackgroundView()
+        chartBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return chartBackgroundView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
         setupRootView()
+        
+        addViews()
+        setupLayouts()
     }
 
     required init?(coder: NSCoder) {
@@ -52,5 +63,77 @@ private extension GraphContainerView {
     func setupRootView() {
         self.backgroundColor = .clear
         self.layer.borderWidth = Configuration.borderWidth
+    }
+    
+    func setupLayouts() {
+        NSLayoutConstraint.activate([
+            chartBackgroundView.topAnchor.constraint(equalTo: self.topAnchor, constant: Configuration.edgeDistance),
+            chartBackgroundView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -Configuration.edgeDistance),
+            chartBackgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Configuration.edgeDistance),
+            chartBackgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Configuration.edgeDistance)
+        ])
+    }
+
+    func addViews() {
+        addSubview(chartBackgroundView)
+    }
+}
+
+final class GraphBackgroundView: UIView {
+    private enum Configuration {
+        static let lineColor = UIColor.black
+        static let lineWidth: CGFloat = 1.0
+        
+        static let numberOfLines = 7
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .clear
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        drawBackgroundView()
+    }
+}
+
+private extension GraphBackgroundView {
+    func drawBackgroundView() {
+        let path = UIBezierPath()
+        path.lineWidth = Configuration.lineWidth
+        Configuration.lineColor.setStroke()
+
+        let horizontalLineInterval = bounds.height / CGFloat(Configuration.numberOfLines)
+        let verticalLineInterval = bounds.width / CGFloat(Configuration.numberOfLines)
+
+        drawHorizontalLines(Configuration.numberOfLines, interval: horizontalLineInterval, with: path)
+        drawVerticalLines(Configuration.numberOfLines, interval: verticalLineInterval, with: path)
+        
+        path.stroke()
+    }
+    
+    func drawHorizontalLines(_ numberOfLines: Int, interval: CGFloat, with path: UIBezierPath) {
+        var y: CGFloat = 0.0
+        for _ in 0...numberOfLines {
+            path.move(to: CGPoint(x: 0.0, y: y))
+            path.addLine(to: CGPoint(x: bounds.width, y: y))
+            y += interval
+        }
+    }
+    
+    func drawVerticalLines(_ numberOfLines: Int, interval: CGFloat, with path: UIBezierPath) {
+        var x: CGFloat = 0.0
+        for _ in 0...numberOfLines {
+            path.move(to: CGPoint(x: x, y: 0.0))
+            path.addLine(to: CGPoint(x: x, y: bounds.height))
+            x += interval
+        }
     }
 }
