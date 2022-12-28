978c1d0fea5a0aba092f4c6977961c1d1b4e716f
Neph <tony3322@naver.com>
2022-12-28 22:36:52 +0900

    [Feat] #12 CoreDataStorage 제작

---
+++ GyroData/CoreDataStorage.swift
@@ -0,0 +1,44 @@
+//
+//  CoreDataStorage.swift
+//  GyroData
+//
+//  Created by 천수현 on 2022/12/28.
+//
+
+import CoreData
+
+final class CoreDataStorage {
+
+    static let shared = CoreDataStorage()
+
+    private init() {}
+
+    // MARK: - Core Data stack
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MotionData")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                // TODO: - Log to Crashlytics
+                assertionFailure("MotionData Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // TODO: - Log to Crashlytics
+                assertionFailure("MotionData Unresolved error \(error), \((error as NSError).userInfo)")
+            }
+        }
+    }
+
+    func performBackgroundTask(_ block: @escaping (NSManagedObjectContext) -> Void) {
+        persistentContainer.performBackgroundTask(block)
+    }
+}
