1043815c56ec2a32fdb76b6238044e0cd502ce40
Neph <tony3322@naver.com>
2022-12-28 11:42:25 +0900

    [Feat] #3 FetchMotionDataListUseCase 파일 생성

---
+++ GyroData/FetchMotionDataListUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  FetchMotionDataListUseCase.swift
+//  GyroData
+//
+//  Created by 천수현 on 2022/12/28.
+//
+
+import Foundation
+
+final class FetchMotionDataListUseCase {
+
+    func execute(completion: @escaping (Result<[MotionRecord], Error>) -> Void) {
+
+    }
+}
