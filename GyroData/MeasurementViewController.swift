3af0349a1ab0d353a8859ab983e2baf8f34bf0b1
Judy <joo00ee@naver.com>
2022-12-28 15:29:08 +0900

    fix: MeasurementVC에 측정, 멈춤 버튼 addView #2

--- GyroData/MeasurementViewController.swift
+++ GyroData/MeasurementViewController.swift
@@ -66,6 +66,8 @@ class MeasurementViewController: UIViewController {
     private func addSubViews() {
         entireStackView.addArrangedSubview(segmentedControl)
         entireStackView.addArrangedSubview(graphView)
+        entireStackView.addArrangedSubview(measurementButton)
+        entireStackView.addArrangedSubview(stopButton)
         
         view.addSubview(entireStackView)
     }
