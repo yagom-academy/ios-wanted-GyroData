a2ff533a6fb744c3c2d4c9f6318e7dcaac6cb601
wongbingg <wb277@naver.com>
2022-12-28 19:58:39 +0900

    feat : CoreDataManager 기능 구현
    - 생성, 읽어오기, 삭제 기능 구현

--- GyroData/Manager/CoreDataManager.swift
+++ GyroData/Manager/CoreDataManager.swift
@@ -5,24 +5,91 @@
 //  Created by minsson on 2022/12/27.
 //
 
+import CoreData
+
 protocol LocalDatabase {
+    var fileManager: CoreDataManagerDelegate? { get set }
     func create(data: MeasuredData)
     func read() -> [MeasuredData]
     func delete(data: MeasuredData)
 }
 
 final class CoreDataManager: LocalDatabase {
-    var data: [MeasuredData] = []
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MotionData")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    weak var fileManager: CoreDataManagerDelegate?
     
     func create(data: MeasuredData) {
+        let context = persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "MotionData", in: context)
+        if let entity = entity {
+            let object = NSManagedObject(entity: entity, insertInto: context)
+            object.setValue(data.uuid, forKey: "uuid")
+            object.setValue(data.date, forKey: "date")
+            object.setValue(data.measuredTime, forKey: "measuredTime")
+            object.setValue(data.sensor.rawValue, forKey: "sensor")
+        }
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
         
+        fileManager?.create(data: data.sensorData, uuid: data.uuid)
     }
     
     func read() -> [MeasuredData] {
-        return data
+        let context = persistentContainer.viewContext
+        do {
+            let motionDataList = try context.fetch(MotionData.fetchRequest()) as! [MotionData]
+            let measuredDataList = motionDataList.map { motionData in
+                let sensorData = fileManager?.read(uuid: motionData.uuid)
+                return MeasuredData(
+                    uuid: motionData.uuid,
+                    date: motionData.date,
+                    measuredTime: motionData.measuredTime,
+                    sensor: .init(rawValue: motionData.sensor)!,
+                    sensorData: sensorData!
+                )
+            }
+            return measuredDataList
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
     }
     
     func delete(data: MeasuredData) {
-        
+        let context = persistentContainer.viewContext
+        guard let motionData = retrieveManagedObejct(from: data) else { return }
+        context.delete(motionData)
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+        fileManager?.delete(uuid: data.uuid)
+    }
+    
+    private func retrieveManagedObejct(from data: MeasuredData) -> MotionData? {
+        let context = persistentContainer.viewContext
+        let request: NSFetchRequest<MotionData> = MotionData.fetchRequest()
+        let predicate = NSPredicate(format: "uuid == %@", data.uuid as CVarArg)
+        request.predicate = predicate
+        do {
+            let result = try context.fetch(request)
+            return result[0]
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
     }
 }
