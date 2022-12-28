10f4b853cd01203e5980e87a775c086c9e0e80db
unchain123 <unchain123@kakao.com>
2022-12-28 19:36:51 +0900

    refactor: 불필요한 extension삭제

--- GyroData/Domain/TableView/AnalyzeListViewController.swift
+++ GyroData/Domain/TableView/AnalyzeListViewController.swift
@@ -77,9 +77,3 @@ extension AnalyzeListViewController: UITableViewDelegate, UITableViewDataSource 
         return UISwipeActionsConfiguration(actions: [delete, play])
     }
 }
-
-extension UIColor {
-    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) {
-        self.init(red: r/255, green: g/255, blue: b/255, alpha: a)
-    }
-}
