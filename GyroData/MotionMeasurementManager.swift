82d5b5149cea28f9bb48de92cb0059d74c347d1a
Judy <joo00ee@naver.com>
2022-12-28 15:31:22 +0900

    feat: 정지 버튼에 타이머와 측정을 멈추는 action 추가 #6

--- GyroData/MotionMeasurementManager.swift
+++ GyroData/MotionMeasurementManager.swift
@@ -10,6 +10,7 @@ import CoreMotion
 class MotionMeasurementManager {
     static let shared = MotionMeasurementManager()
     private let motionManager = CMMotionManager()
+    private var timer: Timer?
     
     private init() {}
     
@@ -20,7 +21,7 @@ class MotionMeasurementManager {
         motionManager.startAccelerometerUpdates()
         
         var timeCount = 0.0
-        let timer = Timer(fire: Date(), interval: 1.0 / 10.0, repeats: true, block: { (timer) in
+        timer = Timer(fire: Date(), interval: 1.0 / 10.0, repeats: true, block: { (timer) in
             timeCount += 0.1
             if (round(timeCount * 10) / 10) == 60.0 {
                 timer.invalidate()
@@ -35,12 +36,15 @@ class MotionMeasurementManager {
             }
         })
         
-        RunLoop.current.add(timer, forMode: .default)
+        if let timer = timer {
+            RunLoop.current.add(timer, forMode: .default)
+        }
     }
     
     func stopUpdates() {
         guard motionManager.isAccelerometerAvailable else { return }
         
         motionManager.stopAccelerometerUpdates()
+        timer?.invalidate()
     }
 }
