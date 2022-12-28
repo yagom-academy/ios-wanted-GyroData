41807bc5f4920a9a9e1106e673cd54680836e2c1
llghdud921 <llghdud921@naver.com>
2022-12-29 01:30:27 +0900

    feat: MotionResultViewModel 구현 #14

---
+++ GyroData/Scenes/MotionResultScene/MotionResultViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  MotionResultViewModel.swift
+//  GyroData
+//
+//  Created by 이호영 on 2022/12/29.
+//
+
+import Foundation
+
+protocol MotionResultViewModelInput {
+    func load()
+}
+
+protocol MotionResultViewModelOutput {
+    var motionData: Observable<[MotionData]> { get }
+}
+
+protocol MotionResultViewModelType: MotionResultViewModelInput, MotionResultViewModelOutput { }
+
+class MotionResultViewModel: MotionResultViewModelType {
+   
+    let sampleData = [MotionData(id: UUID(), motionType: .gyro, date: Date(), time: 60.0, xData: [1.23232323, 1.55555555, -2.434343343], yData: [0.2111112, -1.20000005555, 1.434343343], zData: [-1.23232323, 3.001115555, 2.434343343])]
+    
+    init(_ motion: Motion) {
+        self.motionId = motion.id
+    }
+    
+    /// Output
+    
+    var motionId: UUID?
+    var motionData: Observable<[MotionData]> = Observable([])
+    
+    /// Input
+    
+    func load() {
+        // id 가지고 fileManager 접근
+        // 가지고 온 데이터 motionData에 Set
+        motionData.value = sampleData
+    }
+    
+}
