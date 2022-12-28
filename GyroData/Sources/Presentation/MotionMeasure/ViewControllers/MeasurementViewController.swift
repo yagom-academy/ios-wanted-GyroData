5a9dcc7d1aa98645b3bb5293811673f2371f9719
dnwhd0112 <dnwhd0112@naver.com>
2022-12-28 18:04:05 +0900

    feat: MeasurementViewController 에서 상태값에 따라 뷰가 변화도록 변경

--- GyroData/Sources/Presentation/MotionMeasure/ViewControllers/MeasurementViewController.swift
+++ GyroData/Sources/Presentation/MotionMeasure/ViewControllers/MeasurementViewController.swift
@@ -5,7 +5,6 @@
 //  Created by 우롱차 on 2022/12/27.
 //
 
-import Foundation
 import UIKit
 
 final class MeasurementViewController: UIViewController {
@@ -15,7 +14,21 @@ final class MeasurementViewController: UIViewController {
         static let segmentRightText = "Gyro"
         static let measurementButtonText = "측정"
         static let stopButtonText = "정지"
-        static let buttonColor = UIColor.blue
+        static let saveButtonText = "저장"
+        static let cancleButtonText = "취소"
+        
+        static let graphViewBorderColor = UIColor.gray
+        
+        static let measurementButtonColor = UIColor.green
+        static let stopButtonColor = UIColor.yellow
+        static let saveButtonColor = UIColor.blue
+        static let cancleButtonColor = UIColor.red
+        static let measurementButtonTextColor = UIColor.black
+        static let measurmentButtonTextColor = UIColor.black
+        static let stopButtonTextColor = UIColor.black
+        static let saveButtonTextColor = UIColor.black
+        static let cancleButtonTextColor = UIColor.black
+        static let buttonSize: CGFloat = 80
     }
     
     weak var coordinator: Coordinator?
@@ -42,6 +55,28 @@ final class MeasurementViewController: UIViewController {
         viewModel.currentMotion.observe(on: self) { [weak self] value in
             self?.drawGraph(data: value)
         }
+        viewModel.status.observe(on: self) { [weak self] value in
+            switch value {
+            case .ready:
+                self?.measurementButton.isHidden = false
+                self?.stopButton.isHidden = true
+                self?.saveButton.isHidden = true
+                self?.cancleButton.isEnabled = false
+                self?.segmentControl.isEnabled = true
+            case .start:
+                self?.measurementButton.isHidden = true
+                self?.stopButton.isHidden = false
+                self?.saveButton.isHidden = true
+                self?.cancleButton.isEnabled = true
+                self?.segmentControl.isEnabled = false
+            case .stop:
+                self?.measurementButton.isHidden = true
+                self?.stopButton.isHidden = true
+                self?.saveButton.isHidden = false
+                self?.cancleButton.isEnabled = true
+                self?.segmentControl.isEnabled = false
+            }
+        }
     }
     
     private lazy var segmentControl: UISegmentedControl = {
@@ -54,24 +89,44 @@ final class MeasurementViewController: UIViewController {
     private lazy var graphView: GraphView = {
         let graphView = GraphView()
         graphView.translatesAutoresizingMaskIntoConstraints = false
-        graphView.layer.borderWidth = 3
         return graphView
     }()
     
     private lazy var measurementButton: UIButton = {
-        let button = UIButton()
+        let button = RoundButton(backgroundColor: Constant.measurementButtonColor)
         button.setTitle(Constant.measurementButtonText, for: .normal)
-        button.setTitleColor(Constant.buttonColor, for: .normal)
+        button.setTitleColor(Constant.measurmentButtonTextColor, for: .normal)
         button.addTarget(self, action: #selector(startMeasurement(_:)), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     private lazy var stopButton: UIButton = {
-        let button = UIButton()
+        let button = RoundButton(backgroundColor: Constant.stopButtonColor)
         button.setTitle(Constant.stopButtonText, for: .normal)
-        button.setTitleColor(Constant.buttonColor, for: .normal)
+        button.setTitleColor(Constant.stopButtonTextColor, for: .normal)
         button.addTarget(self, action: #selector(stopMeasurement(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        let button = RoundButton(backgroundColor: Constant.saveButtonColor)
+        button.setTitle(Constant.saveButtonText, for: .normal)
+        button.setTitleColor(Constant.saveButtonTextColor, for: .normal)
+        button.addTarget(self, action: #selector(saveMeasurement(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
+    private lazy var cancleButton: UIButton = {
+        let button = RoundButton(backgroundColor: Constant.cancleButtonColor)
+        button.setTitle(Constant.cancleButtonText, for: .normal)
+        button.setTitleColor(Constant.cancleButtonTextColor, for: .normal)
+        button.addTarget(self, action: #selector(cancleMeasurement(_:)), for: .touchUpInside)
+        button.isEnabled = false
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -99,17 +154,42 @@ final class MeasurementViewController: UIViewController {
         case 0:
             viewModel.measerStop(type: .accelerometer)
         case 1:
-            viewModel.measerStart(type: .gyro)
+            viewModel.measerStop(type: .gyro)
         default:
             return
         }
+    }
+    
+    @objc func saveMeasurement(_ sender: UIButton) {
+        do {
+            switch segmentControl.selectedSegmentIndex {
+            case 0:
+                try? viewModel.measerSave(type: .accelerometer)
+            case 1:
+                try? viewModel.measerSave(type: .gyro)
+            default:
+                return
+            }
+        }
+    }
+    
+    @objc func cancleMeasurement(_ sender: UIButton) {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            viewModel.measerCancle(type: .accelerometer)
+        case 1:
+            viewModel.measerCancle(type: .gyro)
+        default:
+            return
+        }
+        graphView.clean()
     }
 }
 
 extension MeasurementViewController {
     
     private enum ConstantLayout {
-        static let offset: CGFloat = 10
+        static let offset: CGFloat = 30
     }
     
     private func setUpView() {
@@ -128,16 +208,39 @@ extension MeasurementViewController {
             graphView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
         ])
         
+        view.addSubview(cancleButton)
+        NSLayoutConstraint.activate([
+            cancleButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ConstantLayout.offset),
+            cancleButton.topAnchor.constraint(equalTo: graphView.bottomAnchor, constant: ConstantLayout.offset),
+            cancleButton.widthAnchor.constraint(equalToConstant: Constant.buttonSize),
+            cancleButton.heightAnchor.constraint(equalToConstant: Constant.buttonSize)
+        ])
+        
         view.addSubview(measurementButton)
         NSLayoutConstraint.activate([
-            measurementButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ConstantLayout.offset),
-            measurementButton.topAnchor.constraint(equalTo: graphView.bottomAnchor, constant: ConstantLayout.offset)
+            measurementButton.leadingAnchor.constraint(greaterThanOrEqualTo: cancleButton.trailingAnchor),
+            measurementButton.topAnchor.constraint(equalTo: graphView.bottomAnchor, constant: ConstantLayout.offset),
+            measurementButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -ConstantLayout.offset),
+            measurementButton.widthAnchor.constraint(equalToConstant: Constant.buttonSize),
+            measurementButton.heightAnchor.constraint(equalToConstant: Constant.buttonSize)
         ])
         
         view.addSubview(stopButton)
         NSLayoutConstraint.activate([
-            stopButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ConstantLayout.offset),
-            stopButton.topAnchor.constraint(equalTo: measurementButton.bottomAnchor, constant: ConstantLayout.offset)
+            stopButton.leadingAnchor.constraint(greaterThanOrEqualTo: cancleButton.trailingAnchor),
+            stopButton.topAnchor.constraint(equalTo: graphView.bottomAnchor, constant: ConstantLayout.offset),
+            stopButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -ConstantLayout.offset),
+            stopButton.widthAnchor.constraint(equalToConstant: Constant.buttonSize),
+            stopButton.heightAnchor.constraint(equalToConstant: Constant.buttonSize)
+        ])
+        
+        view.addSubview(saveButton)
+        NSLayoutConstraint.activate([
+            saveButton.leadingAnchor.constraint(greaterThanOrEqualTo: cancleButton.trailingAnchor),
+            saveButton.topAnchor.constraint(equalTo: graphView.bottomAnchor, constant: ConstantLayout.offset),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -ConstantLayout.offset),
+            saveButton.widthAnchor.constraint(equalToConstant: Constant.buttonSize),
+            saveButton.heightAnchor.constraint(equalToConstant: Constant.buttonSize)
         ])
     }
 }
