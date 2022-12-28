986e668a6c4c4f7298d366166a37398f28ef806f
minsson <mskaang@gmail.com>
2022-12-28 18:48:17 +0900

    feat : GraphView를 담기 위한 GraphContainerView 구현
    - GraphContainerView는 GraphView, GraphBackgroundView 등을 담기 위한 컨테이너 뷰
    - 기존 GraphView에 UIView 채택, import UIKit이 누락되어 추가함

--- GyroData/ChartView/GraphView.swift
+++ GyroData/ChartView/GraphView.swift
@@ -5,6 +5,8 @@
 //  Created by minsson on 2022/12/27.
 //
 
+import UIKit
+
 protocol GraphDrawable {
     var data: MeasuredData? { get }
     
@@ -18,7 +20,7 @@ protocol TickReceivable {
     func receive(x: Double, y: Double, z: Double)
 }
 
-final class GraphView: TickReceivable {
+final class GraphView: UIView, TickReceivable {
     var data: MeasuredData?
     
     func receive(x: Double, y: Double, z: Double) {
@@ -27,5 +29,28 @@ final class GraphView: TickReceivable {
     
     func retrieveData(data: MeasuredData?) {
         self.data = data
+    }
+}
+
+final class GraphContainerView: UIView {
+    private enum Configuration {
+        static let borderWidth: CGFloat = 4
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupRootView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension GraphContainerView {
+    func setupRootView() {
+        self.backgroundColor = .clear
+        self.layer.borderWidth = Configuration.borderWidth
     }
 }
