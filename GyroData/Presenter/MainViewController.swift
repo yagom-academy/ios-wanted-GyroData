91989b054b43a6c8035b4580c9fee6b1d29aeff3
Tak <oit5538@gmail.com>
2022-12-28 20:31:26 +0900

    feat: viewModel 객체 및 tableView 생성

--- GyroData/Presenter/MainViewController.swift
+++ GyroData/Presenter/MainViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    let viewModel = ViewModel()
+    
+    private let itemTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .white
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "목록"
+    }
+    
+    private func setTableViewLayout() {
+        view.addSubview(itemTableView)
         
+        NSLayoutConstraint.activate([
+            itemTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            itemTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            itemTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            itemTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
 
