f2a066185c0d8994cbf32a3805ba5c94678cb2fb
Judy <joo00ee@naver.com>
2022-12-28 15:25:32 +0900

    feat: MotionMeasurementManager를 Singleton으로 구현 #6
    - 가속도를 측정하고 멈추는 메서드 구현

---
+++ GyroData/MotionMeasurementManager.swift
@@ -0,0 +1,45 @@
+//
+//  MotionMeasurementManager.swift
+//  GyroData
+//
+//  Created by Judy on 2022/12/28.
+//
+
+import CoreMotion
+
+class MotionMeasurementManager {
+    static let shared = MotionMeasurementManager()
+    private let motionManager = CMMotionManager()
+    
+    private init() {}
+    
+    func startAccelerometers(at graphView: GraphView) {
+        guard motionManager.isAccelerometerAvailable else { return }
+        
+        motionManager.accelerometerUpdateInterval = 1.0 / 10.0
+        motionManager.startAccelerometerUpdates()
+        
+        var timeCount = 0.0
+        let timer = Timer(fire: Date(), interval: 1.0 / 10.0, repeats: true, block: { (timer) in
+            timeCount += 0.1
+            if (round(timeCount * 10) / 10) == 60.0 {
+                timer.invalidate()
+            }
+            
+            if let data = self.motionManager.accelerometerData {
+                let x = data.acceleration.x
+                let y = data.acceleration.y
+                let z = data.acceleration.z
+
+            }
+        })
+        
+        RunLoop.current.add(timer, forMode: .default)
+    }
+    
+    func stopUpdates() {
+        guard motionManager.isAccelerometerAvailable else { return }
+        
+        motionManager.stopAccelerometerUpdates()
+    }
+}
