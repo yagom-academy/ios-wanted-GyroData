9281f90d517ccb06648f3863e77f2fd0c67a8b6c
brad <gjrjs31@naver.com>
2022-12-29 21:47:58 +0900

    refactor: CoreMotionManager 기능 수정

--- GyroData/Manager/CoreMotionManager.swift
+++ GyroData/Manager/CoreMotionManager.swift
@@ -11,8 +11,9 @@ import CoreMotion
 protocol SensorDataHandleable {
     var delegate: TickReceivable? { get set }
     
-    func startMeasure(of: Sensor, completion: @escaping (MeasuredData) -> Void)
-    func stopMeasure(of: Sensor) -> MeasuredData
+    func startMeasure(of: Sensor)
+    func stopMeasure()
+    func deliverMeasureData() -> MeasuredData
 }
 
 final class CoreMotionManager: SensorDataHandleable {
@@ -23,6 +24,7 @@ final class CoreMotionManager: SensorDataHandleable {
     private var timer: Timer?
     private var timerNum: Double = 0.0
     private let timeSet: Double = 60.0
+    private var senser: Sensor = Sensor.accelerometer
     private var sensorData: [SensorData] = []
     private var axisX: [Double] = []
     private var axisY: [Double] = []
@@ -32,71 +34,75 @@ final class CoreMotionManager: SensorDataHandleable {
         setupMotionInterval()
     }
     
-    func startMeasure(of SensorType: Sensor, completion: @escaping (MeasuredData) -> Void) {
+    func startMeasure(of SensorType: Sensor) {
         
         if timer != nil && timer!.isValid {
             timer!.invalidate()
         }
-        
         timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { _ in
             self.timerNum += 0.1
-
-            switch SensorType {
-            case .gyro:
-                self.motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
-                    if let myData = data {
-                        self.axisX.append(myData.rotationRate.x.axisDecimal())
-                        self.axisY.append(myData.rotationRate.y.axisDecimal())
-                        self.axisZ.append(myData.rotationRate.z.axisDecimal())
-                        
-                        self.delegate?.receive(
-                            x: myData.rotationRate.x.axisDecimal(),
-                            y: myData.rotationRate.y.axisDecimal(),
-                            z: myData.rotationRate.z.axisDecimal()
-                        )
-                    }
-                    if self.timerNum.timeDecimal() == self.timeSet {
-                        completion(
-                            self.stopMeasure(of: SensorType)
-                        )
-                    }
+            }
+        )
+        switch SensorType {
+        case .gyro:
+            senser = SensorType
+            motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+                if let myData = data {
+                    self.axisX.append(myData.rotationRate.x.axisDecimal())
+                    self.axisY.append(myData.rotationRate.y.axisDecimal())
+                    self.axisZ.append(myData.rotationRate.z.axisDecimal())
+                    
+                    self.delegate?.receive(
+                        x: myData.rotationRate.x.axisDecimal(),
+                        y: myData.rotationRate.y.axisDecimal(),
+                        z: myData.rotationRate.z.axisDecimal()
+                    )
                 }
-            case .accelerometer:
-                self.motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
-                    if let myData = data {
-                        self.axisX.append(myData.acceleration.x.axisDecimal())
-                        self.axisY.append(myData.acceleration.y.axisDecimal())
-                        self.axisZ.append(myData.acceleration.z.axisDecimal())
-                        
-                        self.delegate?.receive(
-                            x: myData.acceleration.x.axisDecimal(),
-                            y: myData.acceleration.y.axisDecimal(),
-                            z: myData.acceleration.z.axisDecimal()
-                        )
-                    }
-                    if self.timerNum.timeDecimal() == self.timeSet {
-                        completion(
-                            self.stopMeasure(of: SensorType)
-                        )
-                    }
+                if self.timerNum.timeDecimal() == self.timeSet {
+                    self.stopMeasure()
                 }
             }
-        })
+        case .accelerometer:
+            senser = SensorType
+            motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+                if let myData = data {
+                    self.axisX.append(myData.acceleration.x.axisDecimal())
+                    self.axisY.append(myData.acceleration.y.axisDecimal())
+                    self.axisZ.append(myData.acceleration.z.axisDecimal())
+                    
+                    self.delegate?.receive(
+                        x: myData.acceleration.x.axisDecimal(),
+                        y: myData.acceleration.y.axisDecimal(),
+                        z: myData.acceleration.z.axisDecimal()
+                    )
+                }
+                if self.timerNum.timeDecimal() == self.timeSet {
+                    self.stopMeasure()
+                }
+            }
+        }
+
     }
     
-    func stopMeasure(of: Sensor) -> MeasuredData {
+    func stopMeasure() {
+        timer?.invalidate()
+        motionManager.stopGyroUpdates()
+        motionManager.stopAccelerometerUpdates()
+    }
+    
+    func deliverMeasureData() -> MeasuredData {
         let mesuredData = MeasuredData(
             uuid: UUID(),
             date: Date(),
             measuredTime: timerNum.timeDecimal(),
-            sensor: of,
+            sensor: senser,
             sensorData: SensorData(
                 axisX: self.axisX,
                 axisY: self.axisY,
                 axisZ: self.axisZ
             )
         )
-        stopSystem()
+        timerNum = 0
         return mesuredData
     }
 }
@@ -107,11 +113,5 @@ private extension CoreMotionManager {
         motionManager.gyroUpdateInterval = 1/60
         motionManager.accelerometerUpdateInterval = 1/60
     }
-    
-    private func stopSystem() {
-        timer?.invalidate()
-        timerNum = 0
-        motionManager.stopGyroUpdates()
-        motionManager.stopAccelerometerUpdates()
-    }
 }
+
