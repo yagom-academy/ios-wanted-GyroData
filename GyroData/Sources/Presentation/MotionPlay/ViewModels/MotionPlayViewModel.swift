0ceae183cc674b5a3b93a34ae2fe797b8210a049
dnwhd0112 <dnwhd0112@naver.com>
2022-12-29 15:18:43 +0900

    fix: 네이밍 수정 및 playStart 함수 변경

--- GyroData/Sources/Presentation/MotionPlay/ViewModels/MotionPlayViewModel.swift
+++ GyroData/Sources/Presentation/MotionPlay/ViewModels/MotionPlayViewModel.swift
@@ -2,7 +2,7 @@
 //  MotionPlayViewModel.swift
 //  GyroData
 //
-//  Created by 곽우종 on 2022/12/28.
+//  Created by 우롱차 on 2022/12/28.
 //
 
 import Foundation
@@ -11,49 +11,63 @@ enum ViewType {
     case play, view
 }
 
-protocol PlayViewModelInput {
+enum PlayStatus {
+    case stop, play
+}
 
-    func playStart(type: MotionType)
+protocol MotionPlayViewModelInput {
+
+    func playStart()
+    func playStop()
     
 }
 
-protocol PlayViewModelOutput {
+protocol MotionPlayViewModelOutput {
     
-    var viewType: Observable<ViewType> { get }
+    var viewType: ViewType { get }
     var motions: Observable<[MotionValue]?> { get }
     var currentMotion: Observable<MotionValue?> { get }
+    var playStatus: Observable<PlayStatus> { get }
     
 }
 
-protocol PlayViewModel: PlayViewModelInput, PlayViewModelOutput {}
+protocol MotionPlayViewModel: MotionPlayViewModelInput, MotionPlayViewModelOutput {}
 
-final class DefaultPlayViewModel: PlayViewModel {
+final class DefaultPlayViewModel: MotionPlayViewModel {
     
-    let motionEntity: MotionEntity
-    
-    var viewType: Observable<ViewType>
-    
+    private let motionEntity: MotionEntity
+    var playStatus: Observable<PlayStatus>
+    var viewType: ViewType
     var motions: Observable<[MotionValue]?>
-    
     var currentMotion: Observable<MotionValue?>
+    
     
     init(motionEntity: MotionEntity, viewtype: ViewType) {
         self.motionEntity = motionEntity
-        self.viewType = Observable(viewtype)
+        self.viewType = viewtype
         let motionList = try? MotionFileManager.shared.load(by: motionEntity.uuid ?? UUID())
         motions =  Observable(motionList?.values)
         currentMotion = Observable(nil)
+        playStatus = Observable(PlayStatus.stop)
     }
     
-    func playStart(type: MotionType) {
+    func playStart() {
+        playStatus.value = .play
         guard let motions = motions.value else {
             return
         }
         for motion in motions {
+            if playStatus.value == .stop {
+                break
+            }
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 self.currentMotion.value = motion
             }
         }
+    }
+    
+    func playStop() {
+        playStatus.value = .stop
     }
 }
 
