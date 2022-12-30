9b9945a780fa7f2908410ac52cc2260c810a4c45
llghdud921 <llghdud921@naver.com>
2022-12-30 20:56:46 +0900

    refactor: motionfileManagerUseCase내 error로직 추가

--- GyroData/Domain/UseCase/MotionFileManagerUseCase.swift
+++ GyroData/Domain/UseCase/MotionFileManagerUseCase.swift
@@ -10,21 +10,20 @@ import Foundation
 class MotionFileManagerUseCase {
     private let fileDataManager = FileDataManager.shared
     
-    func save(_ motion: Motion, motinData: [[Double]]) {
+    func save(_ motion: Motion, motinData: [[Double]], completion: @escaping (Result<Void, FileManagerError>) -> Void) {
         let xData = motinData.map { $0[MotionData.x.rawValue] }
         let yData = motinData.map { $0[MotionData.y.rawValue] }
         let zData = motinData.map { $0[MotionData.z.rawValue] }
         let motionInformation = MotionInformation(motion: motion, xData: xData, yData: yData, zData: zData)
         
-        fileDataManager.save(motionInformation, id: motion.id)
+        fileDataManager.save(motionInformation, id: motion.id, completion: completion)
     }
     
-    func fetch(_ id: UUID) -> MotionInformation? {
-        return fileDataManager.fetch(id)
+    func fetch(_ id: UUID, completion: @escaping (Result<Void, FileManagerError>) -> Void) -> MotionInformation? {
+        return fileDataManager.fetch(id, completion: completion)
     }
     
-    func delete(_ id: UUID) {
-        fileDataManager.delete(id)
+    func delete(_ id: UUID, completion: @escaping (Result<Void, FileManagerError>) -> Void) {
+        fileDataManager.delete(id, completion: completion)
     }
 }
-
