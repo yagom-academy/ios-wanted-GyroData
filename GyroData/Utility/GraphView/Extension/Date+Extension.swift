3cd0c6cbcd952920ca8ecdbfa273e9f2d9488d71
llghdud921 <llghdud921@naver.com>
2022-12-30 16:50:28 +0900

    feat: DateFormatter 적용을 위한 Date extension 추가 #1

---
+++ GyroData/Utility/GraphView/Extension/Date+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Date+Extension.swift
+//  GyroData
+//
+//  Created by 이호영 on 2022/12/30.
+//
+
+import Foundation
+
+extension Date {
+    func convertToString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let dateString = dateFormatter.string(from: self)
+        return dateString
+    }
+}
