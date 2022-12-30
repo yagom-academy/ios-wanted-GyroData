6fafa0c70eb7e95dc6afffea2dd8535d74d26a03
llghdud921 <llghdud921@naver.com>
2022-12-30 16:51:02 +0900

    refactor: date format 변경 #1

--- GyroData/Utility/GraphView/Extension/Date+Extension.swift
+++ GyroData/Utility/GraphView/Extension/Date+Extension.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Date {
     func convertToString() -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
         
         let dateString = dateFormatter.string(from: self)
         return dateString
