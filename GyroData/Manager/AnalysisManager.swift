9bfaffd34798d1c317d4cb856fcd0c06c1b45214
jcrescent61 <jcrescent61@gmail.com>
2022-12-30 16:30:47 +0900

    refactor: AnalyzeManager 내부 로직 수정

--- GyroData/Manager/AnalysisManager.swift
+++ GyroData/Manager/AnalysisManager.swift
@@ -11,14 +11,31 @@ import CoreMotion
 typealias AnalysisData = (x: Double, y: Double, z: Double)
 
 protocol AnalysisManagerType {
-    func startAnalyze() -> AnalysisData
-    func stopAnalyze()
+    func startAnalyze(mode: AnalysisType) -> AnalysisData
+    func stopAnalyze(mode: AnalysisType)
 }
 
 struct AnalysisManager: AnalysisManagerType {
     private let motion = CMMotionManager()
     
-    func startAnalyze() -> AnalysisData {
+    func startAnalyze(mode: AnalysisType) -> AnalysisData {
+        switch mode {
+        case .accelerate:
+            return analyzeAccelerate()
+        case .gyroscope:
+            return analyzeGyroscope()
+        }
+    }
+    
+    func stopAnalyze(mode: AnalysisType) {
+        switch mode {
+        case .accelerate:
+            return stopAnalyzeAccelerate()
+        case .gyroscope:
+            return stopAnalyzeGyroscope()
+        }    }
+    
+    private func analyzeAccelerate() -> AnalysisData {
         guard motion.isAccelerometerAvailable else {
             return (x: 0, y: 0, z: 0)
         }
@@ -31,7 +48,24 @@ struct AnalysisManager: AnalysisManagerType {
         return (x: data.x, y: data.y, z: data.z)
     }
     
-    func stopAnalyze() {
+    private func analyzeGyroscope() -> AnalysisData {
+        guard motion.isGyroAvailable else {
+            return (x: 0, y: 0, z: 0)
+        }
+        motion.gyroUpdateInterval = 0.1
+        motion.startGyroUpdates()
+        
+        guard let data = self.motion.gyroData?.rotationRate else {
+            return (x: 0, y: 0, z: 0)
+        }
+        return (x: data.x, y: data.y, z: data.z)
+    }
+    
+    private func stopAnalyzeAccelerate() {
         motion.stopAccelerometerUpdates()
+    }
+    
+    private func stopAnalyzeGyroscope() {
+        motion.stopGyroUpdates()
     }
 }
