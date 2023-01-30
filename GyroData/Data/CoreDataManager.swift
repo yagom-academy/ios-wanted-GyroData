1f9e202fd42fca0f961cff2fb4ab4bef6814a68d
jpush <lj845854@gmail.com>
2023-01-30 22:25:56 +0900

    feat : coreData save 메서드 구현
    - return 값으로 Result를 반환하여 저장 성공여부와 에러를 반환.
    - 성공 시 Void를 반환

--- GyroData/Data/CoreDataManager.swift
+++ GyroData/Data/CoreDataManager.swift
@@ -5,4 +5,36 @@
 //  Created by Kyo, JPush on 2023/01/30.
 //
 
+import CoreData
 import Foundation
+
+class CoreDataManager {
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "SensorData")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                print(error.localizedDescription)
+            }
+        }
+        return container
+    }()
+    lazy var context = persistentContainer.viewContext
+    
+    func save(_ model: MeasureData) -> Result<Void, Error> {
+        let content = SensorData(context: self.context)
+        
+        content.setValue(model.xValue, forKey: "xValue")
+        content.setValue(model.yValue, forKey: "yValue")
+        content.setValue(model.zValue, forKey: "zValue")
+        content.setValue(model.date, forKey: "date")
+        content.setValue(model.runTime, forKey: "runTime")
+        content.setValue(model.type, forKey: "type")
+        
+        do {
+            try context.save()
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
+    }
+}
