d624fb2682e56242a64016142ff2efab0ba32de1
Mangdi <lkw4670@naver.com>
2023-01-30 20:25:52 +0900

    feat: UITableViewDataSource와 UITableViewDelegate extention

--- GyroData/Resources/Controller/ViewController.swift
+++ GyroData/Resources/Controller/ViewController.swift
@@ -35,3 +35,17 @@ final class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
+
