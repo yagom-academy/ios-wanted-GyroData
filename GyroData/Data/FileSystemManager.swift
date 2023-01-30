9537b97df694ae601da9d9b253f289be10d37bfc
Kyo <hyosing92@gmail.com>
2023-01-31 00:33:48 +0900

    feat : FileSystemManager 내부 fileManager, 파일 저장 Path 프로퍼티 설정

--- GyroData/Data/FileSystemManager.swift
+++ GyroData/Data/FileSystemManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+final class FileSystemManager {
+    private enum FileConstant {
+        static let directoryName = "Sensor_JSON_Folder"
+    }
+    
+    private let fileManager = FileManager.default
+    private let documentPath: URL
+    private let directoryPath: URL
+    
+    init() {
+        documentPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[.zero]
+        directoryPath = documentPath.appendingPathExtension(FileConstant.directoryName)
+    }
+}
