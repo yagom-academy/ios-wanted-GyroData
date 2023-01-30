0c4ce3987fe834a912eb70b59549c42a2554ab2e
Mangdi <lkw4670@naver.com>
2023-01-31 02:37:25 +0900

    feat: CoreDataModel의 NSManagedObject Subclass 생성

---
+++ GyroData/Resources/Model/GyroModelObject+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  GyroModelObject+CoreDataClass.swift
+//  GyroData
+//
+//  Created by Mangdi on 2023/01/31.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(GyroModelObject)
+public class GyroModelObject: NSManagedObject {
+
+}
---
+++ GyroData/Resources/Model/GyroModelObject+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  GyroModelObject+CoreDataProperties.swift
+//  GyroData
+//
+//  Created by Mangdi on 2023/01/31.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension GyroModelObject {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<GyroModelObject> {
+        return NSFetchRequest<GyroModelObject>(entityName: "GyroModelObject")
+    }
+
+    @NSManaged public var saveDate: String?
+    @NSManaged public var sensorType: String?
+    @NSManaged public var recordTime: Double
+    @NSManaged public var jsonName: String?
+    @NSManaged public var id: UUID?
+
+}
+
+extension GyroModelObject : Identifiable {
+
+}
