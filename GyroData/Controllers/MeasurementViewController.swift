f4cf738cd4fcfeffc05d60b66e56ebb5b178d5f9
yuvinrho <yvrho@naver.com>
2023-01-30 22:25:42 +0900

    feat: MeasurementViewController segmentedController 설정

--- GyroData/Controllers/MeasurementViewController.swift
+++ GyroData/Controllers/MeasurementViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 final class MeasurementViewController: UIViewController {
-   private lazy var saveBarButton: UIBarButtonItem = {
+    private lazy var segmentedController: UISegmentedControl = {
+        let controller = UISegmentedControl(items: ["Acc", "Gyro"])
+        controller.translatesAutoresizingMaskIntoConstraints = false
+        controller.addTarget(self, action: #selector(changeSensor(sender:)), for: .valueChanged)
+        controller.selectedSegmentIndex = 0
+        controller.selectedSegmentTintColor = .systemBlue
+        return controller
+    }()
+
+    private var selectedSensor: Sensor {
+        guard let selectedSensor = Sensor(rawValue: segmentedController.selectedSegmentIndex) else { fatalError() }
+        return selectedSensor
+    }
+    
+    private lazy var saveBarButton: UIBarButtonItem = {
         let button = UIBarButtonItem(title: "저장", primaryAction: makeSaveButtonAction())
         return button
     }()
@@ -35,5 +49,9 @@ final class MeasurementViewController: UIViewController {
 
     private func saveSensorData() {
         fatalError()
+    }
+
+    @objc private func changeSensor(sender: UISegmentedControl) {
+        print("선택:\(selectedSensor)")
     }
 }
