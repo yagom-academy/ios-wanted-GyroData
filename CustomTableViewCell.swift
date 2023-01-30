5534c7030293898423661799d6da50ce58d09836
Mangdi <lkw4670@naver.com>
2023-01-30 20:37:43 +0900

    feat: cell에서 나타날 dateView, sensorTypeView, runningTImeView, verticalStackView 생성

--- CustomTableViewCell.swift
+++ CustomTableViewCell.swift
@@ -9,6 +9,36 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
+    // MARK: - Property
+    let dateView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    let sensorTypeView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    let runningTimeView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fill
+        return stackView
+    }()
+
+    // MARK: - Method
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
