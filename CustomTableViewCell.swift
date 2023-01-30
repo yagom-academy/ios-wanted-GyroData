bf1f88f161c1c43b1cd76a2e51901d9a9a24c47f
Mangdi <lkw4670@naver.com>
2023-01-30 21:17:02 +0900

    feat: cell의 UI객체들 레이아웃 제약 잡아주기

--- CustomTableViewCell.swift
+++ CustomTableViewCell.swift
@@ -13,18 +13,21 @@ class CustomTableViewCell: UITableViewCell {
     let dateView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "test"
         return textView
     }()
 
     let sensorTypeView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.systemFont(ofSize: 30)
         return textView
     }()
 
     let runningTimeView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.systemFont(ofSize: 60)
         return textView
     }()
 
@@ -38,12 +41,30 @@ class CustomTableViewCell: UITableViewCell {
     }()
 
     // MARK: - Method
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+        configureSubViews()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureSubViews() {
+        verticalStackView.addArrangedSubview(dateView)
+        verticalStackView.addArrangedSubview(sensorTypeView)
+        contentView.addSubview(verticalStackView)
+        contentView.addSubview(runningTimeView)
+
+        NSLayoutConstraint.activate([
+            verticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            verticalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            verticalStackView.trailingAnchor.constraint(equalTo: runningTimeView.leadingAnchor),
+            verticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            runningTimeView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            runningTimeView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            runningTimeView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
     }
 }
