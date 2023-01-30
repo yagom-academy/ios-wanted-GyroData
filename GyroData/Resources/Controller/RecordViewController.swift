0ab7a8d7d99d623272c445f11410cd089a46bb7e
미니 <cow970814@naver.com>
2023-01-30 17:51:42 +0900

    feat : 데이터 저장 ViewController 생성

--- GyroData/Resources/Controller/RecordViewController.swift
+++ GyroData/Resources/Controller/RecordViewController.swift
@@ -5,4 +5,11 @@
 //  Copyright (c) 2023 Minii All rights reserved.
         
 
-import Foundation
+import UIKit
+
+class RecordViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
