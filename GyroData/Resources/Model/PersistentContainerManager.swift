4da44cf38440db0e9894bfd680ddd72c9aeaba51
Mangdi <lkw4670@naver.com>
2023-01-31 02:56:43 +0900

    feat: PersistentContainerManager 싱글톤 생성

---
+++ GyroData/Resources/Model/PersistentContainerManager.swift
@@ -0,0 +1,13 @@
+//
+//  PersistentContainerManager.swift
+//  GyroData
+//
+//  Created by Mangdi on 2023/01/31.
+//
+
+import CoreData
+
+final class PersistentContainerManager {
+    static let shared = PersistentContainerManager()
+    private init() { }
+}
