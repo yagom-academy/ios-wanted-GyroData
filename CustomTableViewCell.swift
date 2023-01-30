ae53c09e4fa18104b44dfa0a047ad61c859c408a
Mangdi <lkw4670@naver.com>
2023-01-30 20:31:27 +0900

    feat: CustomTableViewCell 파일 생성

---
+++ CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  GyroData
+//
+//  Created by Mangdi on 2023/01/30.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
