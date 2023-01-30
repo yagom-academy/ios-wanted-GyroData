f4c190850ee09b1c07c02cc69eedd69e3c6b4d46
Mangdi <lkw4670@naver.com>
2023-01-30 20:19:26 +0900

    feat: TableView 생성

--- GyroData/Resources/Controller/ViewController.swift
+++ GyroData/Resources/Controller/ViewController.swift
@@ -7,9 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    // MARK: - Property
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    // MARK: - Method
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
 }
 
