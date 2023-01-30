15fa3ecc97b8a9ba57939de76fec6856450a45a5
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-01-30 22:11:43 +0900

    feat: View구현 - ListCell 클래스,  Label들, StackView추가 및 오토레이아웃 제약
    - UILabel, UIStackView: Extension에 편의이니셜라이저 생성

---
+++ GyroData/ListViews/Extension +/UIStackView +.swift
@@ -0,0 +1,29 @@
+//
+//  UIStackView +.swift
+//  GyroData
+//
+//  Created by 써니쿠키, 로빈 on 2023/01/30.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    convenience init(axis: NSLayoutConstraint.Axis = .horizontal,
+                     distribution: UIStackView.Distribution = .fill,
+                     alignment: UIStackView.Alignment = .fill,
+                     spacing: CGFloat = 0,
+                     margin: CGFloat = .zero) {
+        self.init(frame: .zero)
+        self.axis = axis
+        self.distribution = distribution
+        self.alignment = alignment
+        self.spacing = spacing
+        self.layoutMargins = UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if layoutMargins != .zero {
+            isLayoutMarginsRelativeArrangement = true
+        }
+    }
+}
---
+++ GyroData/ListViews/ListCell.swift
@@ -0,0 +1,61 @@
+//
+//  ListCell.swift
+//  GyroData
+//
+//  Created by 써니쿠키, 로빈 on 2023/01/30.
+//
+
+import UIKit
+
+final class ListCell: UITableViewCell {
+    
+    static var reuseIdentifier: String {
+           return String(describing: self)
+       }
+    
+    private let dateLabel = UILabel(font: .body)
+    private let sensorNameLabel = UILabel(font: .title2)
+    private let valueLabel = UILabel(font: .largeTitle, textAlignment: .center)
+    private let verticalStackView = UIStackView(axis: .vertical,
+                                                distribution: .fill,
+                                                alignment: .fill,
+                                                spacing: 20)
+    private let totalStackView = UIStackView(distribution: .fill,
+                                             alignment: .center,
+                                             margin: 20)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+        
+
+    }
+    
+    private func configureHierarchy() {
+        [dateLabel, sensorNameLabel].forEach { label in
+            verticalStackView.addArrangedSubview(label)
+        }
+        
+        [verticalStackView, valueLabel].forEach { view in
+            totalStackView.addArrangedSubview(view)
+        }
+        
+        contentView.addSubview(totalStackView)
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            totalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            totalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            totalStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            totalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            valueLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
