263225882db9fd396a2c8d5c29ca913cfc6355d4
yuvinrho <yvrho@naver.com>
2023-01-30 22:18:27 +0900

    feat: Accelerometer, Gyro Manager객체 정의
    - 내부로직 미구현

---
+++ GyroData/Models/AccelerometerManager.swift
@@ -0,0 +1,19 @@
+//
+//  AccelerometerManager.swift
+//  GyroData
+//
+//  Created by 로빈 on 2023/01/30.
+//
+
+import Foundation
+import CoreMotion
+
+class AccelerometerManager {
+    func measure() {
+        print("가속도계 측정")
+    }
+
+    func stop() {
+        print("가속도계 측정정지")
+    }
+}
---
+++ GyroData/Models/GyroManager.swift
@@ -0,0 +1,19 @@
+//
+//  GyroManager.swift
+//  GyroData
+//
+//  Created by 로빈 on 2023/01/30.
+//
+
+import Foundation
+import CoreMotion
+
+class GyroManager {
+    func measure() {
+        print("자이로 측정")
+    }
+
+    func stop() {
+        print("자이로 측정정지")
+    }
+}
