532d36ac87c4f5ceb10b5f4061c72bbc50d71ede
yuvinrho <yvrho@naver.com>
2023-01-30 22:34:43 +0900

    feat: MeasurementViewController 측정, 정지 버튼 설정

--- GyroData/Controllers/MeasurementViewController.swift
+++ GyroData/Controllers/MeasurementViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 final class MeasurementViewController: UIViewController {
+    // MARK: View Properties
     private lazy var segmentedController: UISegmentedControl = {
         let controller = UISegmentedControl(items: ["Acc", "Gyro"])
         controller.translatesAutoresizingMaskIntoConstraints = false
@@ -17,21 +18,52 @@ final class MeasurementViewController: UIViewController {
         return controller
     }()
 
-    private var selectedSensor: Sensor {
-        guard let selectedSensor = Sensor(rawValue: segmentedController.selectedSegmentIndex) else { fatalError() }
-        return selectedSensor
-    }
-    
     private lazy var saveBarButton: UIBarButtonItem = {
         let button = UIBarButtonItem(title: "저장", primaryAction: makeSaveButtonAction())
         return button
     }()
+
+    private lazy var measurementButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("측정", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addAction(makeMeasurementButtonAction(), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var stopButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("정지", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addAction(makeStopButtonAction(), for: .touchUpInside)
+        return button
+    }()
+
+    // MARK: Properties
+    private let measurementManager = MeasurementManager()
+    private var selectedSensor: Sensor {
+        guard let selectedSensor = Sensor(rawValue: segmentedController.selectedSegmentIndex) else { fatalError() }
+        return selectedSensor
+    }
 
     // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationBar()
+    }
+
+    // MARK: NavigationBar
+    private func configureNavigationBar() {
+        navigationItem.title = "측정하기"
+        navigationItem.rightBarButtonItem = saveBarButton
+    }
+
+    // MARK: Actions
+    @objc private func changeSensor(sender: UISegmentedControl) {
+        print("선택:\(selectedSensor)")
     }
 
     private func makeSaveButtonAction() -> UIAction {
@@ -42,16 +74,32 @@ final class MeasurementViewController: UIViewController {
         return action
     }
 
-    private func configureNavigationBar() {
-        navigationItem.title = "측정하기"
-        navigationItem.rightBarButtonItem = saveBarButton
+    private func makeMeasurementButtonAction() -> UIAction {
+        let action = UIAction { [weak self] _ in
+            self?.measure()
+        }
+
+        return action
+    }
+
+    private func makeStopButtonAction() -> UIAction {
+        let action = UIAction { [weak self] _ in
+            self?.measureStop()
+        }
+
+        return action
+    }
+
+    // MARK: Methods
+    private func measure() {
+        measurementManager.measure(sensor: selectedSensor)
+    }
+
+    private func measureStop() {
+        measurementManager.stop(sensor: selectedSensor)
     }
 
     private func saveSensorData() {
         fatalError()
-    }
-
-    @objc private func changeSensor(sender: UISegmentedControl) {
-        print("선택:\(selectedSensor)")
     }
 }
