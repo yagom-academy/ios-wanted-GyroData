1568358c23d3c48a583bf95b2d56f21292877131
jpush <lj845854@gmail.com>
2023-01-30 21:08:40 +0900

    config: 베이스 폴더 생성

---
+++ GyroData/Data/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  GyroData
+//
+//  Created by 이정민 on 2023/01/30.
+//
+
+import Foundation
---
+++ GyroData/Domain/Entities/Sensor.swift
@@ -0,0 +1,8 @@
+//
+//  Sensor.swift
+//  GyroData
+//
+//  Created by 이정민 on 2023/01/30.
+//
+
+import Foundation
