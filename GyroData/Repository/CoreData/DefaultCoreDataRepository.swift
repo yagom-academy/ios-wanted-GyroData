4e2ae4a7ab61b6a59aeff686a2b167eb697fd87f
wonbi92 <bin9239@gmail.com>
2023-01-31 21:09:22 +0900

    feat: DefaultCoreDataRepository 구조체 및 CoreDataError 열거형 구현
    - CoreDataRepository 프로토콜 채택
    - persistentContainer, context 프로퍼티 구현

--- GyroData/Repository/CoreData/DefaultCoreDataRepository.swift
+++ GyroData/Repository/CoreData/DefaultCoreDataRepository.swift
@@ -5,4 +5,43 @@
 //  Created by Ayaan, Wonbi on 2023/01/31.
 //
 
-import Foundation
+import CoreData
+
+enum CoreDataError: Error {
+    case invalidEntity
+}
+
+struct DefaultCoreDataRepository: CoreDataRepository {
+    typealias Domain = Motion
+    typealias Entity = MotionMO
+    
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "GyroData")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func create(_ domain: Motion) throws {
+        
+    }
+    
+    func read(from offset: Int) throws -> [MotionMO] {
+        return []
+    }
+    
+    func read(with id: String) throws -> MotionMO {
+        return MotionMO(context: context)
+    }
+    
+    func delete(_ id: String) throws {
+        
+    }
+}
