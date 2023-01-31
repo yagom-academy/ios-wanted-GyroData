89787357703be92a6a2e876e617766c01a980fe3
wonbi92 <bin9239@gmail.com>
2023-01-31 23:05:39 +0900

    chore: MotionMO+ 컨벤션 수정

--- GyroData/Repository/CoreData/Entity/MotionMO+.swift
+++ GyroData/Repository/CoreData/Entity/MotionMO+.swift
@@ -10,12 +10,16 @@ import Foundation
 extension MotionMO: DomainConvertible {
     func asDomain() -> Motion? {
         guard let type = Motion.MeasurementType.init(rawValue: Int(self.type)) else { return nil }
-        return Motion(id: self.id,
-                      date: Date(timeIntervalSince1970: self.date),
-                      type: type,
-                      time: self.time,
-                      data: Motion.MeasurementData(x: self.x,
-                                                   y: self.y,
-                                                   z: self.z))
+        return Motion(
+            id: self.id,
+            date: Date(timeIntervalSince1970: self.date),
+            type: type,
+            time: self.time,
+            data: Motion.MeasurementData(
+                x: self.x,
+                y: self.y,
+                z: self.z
+            )
+        )
     }
 }
