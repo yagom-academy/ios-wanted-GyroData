156ba2ad173213d09414f40ab7feb84f1d29e7b3
wonbi92 <bin9239@gmail.com>
2023-01-31 16:14:07 +0900

    feat: Motion 구조체 구현 및 Identifiable 채택
    - MeasurementData 타입 구현
    - MeasurementType 타입 구현

--- GyroData/Domain/Entity/Motion.swift
+++ GyroData/Domain/Entity/Motion.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct Motion: Identifiable {
+    struct MeasurementData {
+        var x: [Double]
+        var y: [Double]
+        var z: [Double]
+        
+        func append(x: Double, y: Double, z: Double) {
+            
+        }
+    }
+    
+    enum MeasurementType: Int {
+        case acc = 0
+        case gyro
+        
+        var text: String {
+            switch self {
+            case .acc:
+                return "Accelerometer"
+            case .gyro:
+                return "Gyro"
+            }
+        }
+    }
+    
+    let id: String
+    let date: Date
+    let type: MeasurementType
+    let time: Double
+    var data: MeasurementData
+}
