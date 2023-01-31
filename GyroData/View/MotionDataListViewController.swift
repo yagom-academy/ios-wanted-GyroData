a693227966b6492b9f3db56add478d57b6d597f7
junho <junhoL1046@gmail.com>
2023-01-31 21:13:28 +0900

    feat: MotionDataListViewController 추가하고 RootViewController로 설정

---
+++ GyroData/View/MotionDataListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MotionDataListViewController.swift
+//  GyroData
+//
+//  Created by junho lee on 2023/01/31.
+//
+
+import UIKit
+
+class MotionDataListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
