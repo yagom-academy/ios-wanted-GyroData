70c946c6c76e5222a85ddb4bbb09ec337918fd5e
summercat <summerly812@gmail.com>
2023-01-31 22:26:52 +0900

    feat: RecordMotionDataVC에 viewModel 프로퍼티, 이니셜라이저 추가

--- GyroData/View/RecordMotionDataViewController.swift
+++ GyroData/View/RecordMotionDataViewController.swift
@@ -7,23 +7,19 @@
 
 import UIKit
 
-class RecordMotionDataViewController: UIViewController {
+final class RecordMotionDataViewController: UIViewController {
+    private let viewModel: RecordMotionDataViewModel
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    init(viewModel: RecordMotionDataViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
