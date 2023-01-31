d68f17ab98cff2c2119a9f47e3ba9f03c07806cc
junho <junhoL1046@gmail.com>
2023-01-31 12:58:20 +0900

    feat: id 프로퍼티, 이니셜라이저 추가

--- GyroData/Model/MotionData.swift
+++ GyroData/Model/MotionData.swift
@@ -8,8 +8,21 @@
 import Foundation
 
 struct MotionData {
+    let id: UUID
     var createdAt: Date
     var length: Double
     var motionDataType: MotionDataType
     var coordinates: [Coordinate]
+
+    init(id: UUID = UUID(),
+         createdAt: Date = Date(),
+         length: Double = 0,
+         motionDataType: MotionDataType,
+         coordinates: [Coordinate] = []) {
+        self.id = id
+        self.createdAt = createdAt
+        self.length = length
+        self.motionDataType = motionDataType
+        self.coordinates = coordinates
+    }
 }
