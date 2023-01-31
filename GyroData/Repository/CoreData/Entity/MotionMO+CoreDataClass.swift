3702497b2df1ee264442473f3467620f289d0146
Ayaan <onestar7984@gmail.com>
2023-01-31 15:42:58 +0900

    feat: CoreData Model 및 MotionMO Entity 구현

---
+++ GyroData/Repository/CoreData/Entity/MotionMO+CoreDataClass.swift
@@ -0,0 +1,11 @@
+//
+//  MotionMO+CoreDataClass.swift
+//  GyroData
+//
+//  Created by Ayaan, Wonbi on 2023/01/31.
+//
+
+import CoreData
+
+@objc(MotionMO)
+public final class MotionMO: NSManagedObject { }
