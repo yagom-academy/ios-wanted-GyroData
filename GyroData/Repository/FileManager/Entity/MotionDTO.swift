7d290db175035217ccf9fbdf9ae1b630d28aae74
wonbi92 <bin9239@gmail.com>
2023-01-31 15:53:23 +0900

    feat: MotionDTO 구조체 구현 및 Codable, Identifiable 채택

--- GyroData/Repository/FileManager/Entity/MotionDTO.swift
+++ GyroData/Repository/FileManager/Entity/MotionDTO.swift
@@ -5,4 +5,12 @@
 //  Created by Ayaan, Wonbi on 2023/01/31.
 //
 
-import Foundation
+struct MotionDTO: Codable, Identifiable {
+    let id: String
+    let date: Double
+    let type: Int
+    let time: Double
+    let x: [Double]
+    let y: [Double]
+    let z: [Double]
+}
