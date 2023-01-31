941e0665c81b570f46c726ee017baff971ab9911
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-01-31 18:52:20 +0900

    feat: ListViewController - Paging구현, Cell selectedColor유지 제거,

--- GyroData/Controllers/ListViewController.swift
+++ GyroData/Controllers/ListViewController.swift
@@ -16,6 +16,7 @@ final class ListViewController: UIViewController {
     private let listView: ListView = ListView()
     private var dataSource: DataSource? = nil
     private var snapShot: SnapShot = SnapShot()
+    private var isPaging: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +55,7 @@ final class ListViewController: UIViewController {
     }
     
     private func applySnapshot() {
+        snapShot.deleteAllItems()
         snapShot.appendSections([.main])
         snapShot.appendItems(measurements)
         dataSource?.apply(snapShot)
@@ -66,7 +68,7 @@ extension ListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView,
                    trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath)
     -> UISwipeActionsConfiguration? {
-       
+        
         let playAction = UIContextualAction(style: .normal, title: "Play") { _, _, _ in
             print("데이터 Play")
             // 액션 구현 예정
@@ -79,6 +81,34 @@ extension ListViewController: UITableViewDelegate {
         }
         
         return UISwipeActionsConfiguration(actions: [deleteAction, playAction])
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.isSelected = false
+        presentDetailPage()
+    }
+    
+    private func presentDetailPage() {
+        //구현예정: 3번째 페이지연결
+        print("자세히보기")
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let frameHeight = scrollView.frame.height
+        
+        guard isPaging == false,
+              offsetY > contentHeight - frameHeight else {
+            return
+        }
+        
+        isPaging = true
+        // 구현예정
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            print("paging")
+        }
+        isPaging = false
     }
 }
 
