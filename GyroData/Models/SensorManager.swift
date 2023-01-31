4874615647ebbfcfd4fe2745008758f7d4987dda
yuvinrho <yvrho@naver.com>
2023-01-31 14:09:23 +0900

    refactor: SensorManager 리팩토링

--- GyroData/Models/SensorManager.swift
+++ GyroData/Models/SensorManager.swift
@@ -11,9 +11,11 @@ class SensorManager {
     private let motionManager = CMMotionManager()
     private var timer = Timer()
 
-    func measure(sensor: Sensor, interval: TimeInterval, timeout: TimeInterval, completion: @escaping (Axis) -> ()) {
+    func measure(sensor: Sensor, interval: TimeInterval, timeout: TimeInterval, completion: @escaping (AxisData?) -> ()) {
         timer = Timer.scheduledTimer(withTimeInterval: timeout * interval, repeats: false) { [weak self] _ in
-            self?.stop()
+            self?.stop { _ in
+                completion(nil)
+            }
         }
 
         switch sensor {
@@ -28,7 +30,7 @@ class SensorManager {
         }
     }
 
-    func stop() {
+    func stop(completion: @escaping (Bool) -> ()) {
         if motionManager.isAccelerometerActive {
             motionManager.stopAccelerometerUpdates()
         }
@@ -38,23 +40,24 @@ class SensorManager {
         }
 
         timer.invalidate()
+        completion(true)
     }
 
-    private func fetchAccelerometerData(interval: TimeInterval, completion: @escaping (Axis) -> ()) {
+    private func fetchAccelerometerData(interval: TimeInterval, completion: @escaping (AxisData) -> ()) {
         motionManager.accelerometerUpdateInterval = interval
         motionManager.startAccelerometerUpdates(to: .main) { accelerometerData, _ in
             guard let acceleration = accelerometerData?.acceleration else { return }
             print(acceleration)
-            completion(Axis(x: acceleration.x, y: acceleration.y, z: acceleration.z))
+            completion(AxisData(x: acceleration.x, y: acceleration.y, z: acceleration.z))
         }
     }
 
-    private func fetchGyroData(interval: TimeInterval, completion: @escaping (Axis) -> ()) {
+    private func fetchGyroData(interval: TimeInterval, completion: @escaping (AxisData) -> ()) {
         motionManager.gyroUpdateInterval = interval
         motionManager.startGyroUpdates(to: .main) { gyroData, _ in
             guard let rotationRate = gyroData?.rotationRate else { return }
             print(rotationRate)
-            completion(Axis(x: rotationRate.x, y: rotationRate.y, z: rotationRate.z))
+            completion(AxisData(x: rotationRate.x, y: rotationRate.y, z: rotationRate.z))
         }
     }
 }
