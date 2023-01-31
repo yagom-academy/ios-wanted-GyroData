9cf34925cf0138a2388b8eb17242b14280fd2400
Kyo <hyosing92@gmail.com>
2023-01-31 20:11:56 +0900

    feat : TransactionService dataLoad(), jsonDataLoad() 메서드 구현

--- GyroData/Source/Domain/TransactionService.swift
+++ GyroData/Source/Domain/TransactionService.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+final class TransactionService {
+    private var list: [MeasureData] = []
+    
+    private let coreDataManager: CoreDataManager
+    private let fileManager: FileSystemManager
+    
+    init(coreDataManager: CoreDataManager, fileManager: FileSystemManager) {
+        self.coreDataManager = coreDataManager
+        self.fileManager = fileManager
+    }
+}
+
+extension TransactionService {
+    func dataLoad(offset: Int, limit: Int) -> Result<[MeasureData], Error> {
+        do {
+            list = try coreDataManager.fetch(offset: offset, limit: limit)
+            return .success(list)
+        } catch {
+            return .failure(error)
+        }
+        
+    }
+    
+    func jsonDataLoad(date: Date) -> Result<Data, FileSystemError> {
+        do {
+            let data = try fileManager.fetch(date: date)
+            return .success(data)
+        } catch {
+            return .failure(.loadError)
+        }
+    }
+}
