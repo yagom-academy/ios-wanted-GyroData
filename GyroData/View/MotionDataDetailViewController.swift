f92d0d9ac52b7ed93b868a1e9d23450cc323be2d
summercat <summerly812@gmail.com>
2023-01-31 22:32:06 +0900

    refactor: MotionDataDetail로 네이밍 수정

---
+++ GyroData/View/MotionDataDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MotionDataDetailViewController.swift
+//  GyroData
+//
+//  Created by Jiyoung Lee on 2023/01/31.
+//
+
+import UIKit
+
+class MotionDataDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
