6e957efb503f3aabea74af21de62d8f832efcf53
zhilly11 <zhzh1x4@gmail.com>
2023-02-01 13:14:06 +0900

    feat: CoreMotionManager 타입 구현

---
+++ GyroData/Manager/CoreMotionManager.swift
@@ -0,0 +1,64 @@
+//  GyroData - CoreMotionManager.swift
+//  Created by zhilly, woong on 2023/02/01
+
+import Foundation
+import CoreMotion
+
+final class CoreMotionManager {
+    private var motion = CMMotionManager()
+    private var timer: Timer?
+    
+    func startGyros() {
+        if motion.isGyroAvailable {
+            self.motion.gyroUpdateInterval = 1.0 / 60.0
+            self.motion.startGyroUpdates()
+            
+            self.timer = Timer(fire: Date(),
+                               interval: (10.0 / 60.0),
+                               repeats: true,
+                               block: { (timer) in
+                
+                if let data = self.motion.gyroData {
+                    let x = data.rotationRate.x
+                    let y = data.rotationRate.y
+                    let z = data.rotationRate.z
+                    
+                    print("\(x), \(y), \(z)")
+                }
+            })
+            
+            RunLoop.current.add(self.timer!, forMode: .default)
+        }
+    }
+    
+    func stopGyros() {
+        if self.timer != nil {
+            self.timer?.invalidate()
+            self.timer = nil
+            
+            self.motion.stopGyroUpdates()
+        }
+    }
+    
+    func startAccelerometers() {
+        if self.motion.isAccelerometerAvailable {
+            self.motion.accelerometerUpdateInterval = 1.0 / 60.0
+            self.motion.startAccelerometerUpdates()
+            
+            self.timer = Timer(fire: Date(),
+                               interval: (10.0 / 60.0),
+                               repeats: true,
+                               block: { (timer) in
+                if let data = self.motion.accelerometerData {
+                    let x = data.acceleration.x
+                    let y = data.acceleration.y
+                    let z = data.acceleration.z
+                    
+                    print("\(x), \(y), \(z)")
+                }
+            })
+            
+            RunLoop.current.add(self.timer!, forMode: .default)
+        }
+    }
+}
