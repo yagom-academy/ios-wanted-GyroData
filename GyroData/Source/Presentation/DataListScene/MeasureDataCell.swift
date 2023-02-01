ede511955caad63aa41252905edc88f728b169e9
Kyo <hyosing92@gmail.com>
2023-02-01 18:04:29 +0900

    refactor : MeasureDataCell Base Method 생성

--- GyroData/Source/Presentation/DataListScene/MeasureDataCell.swift
+++ GyroData/Source/Presentation/DataListScene/MeasureDataCell.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 
-class MeasureDataCell: UITableViewCell {
+final class MeasureDataCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        setupConstraint()
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+extension MeasureDataCell {
+    private func setupView() {
+        
+    }
+    
+    private func setupConstraint() {
+        
     }
 }
 
