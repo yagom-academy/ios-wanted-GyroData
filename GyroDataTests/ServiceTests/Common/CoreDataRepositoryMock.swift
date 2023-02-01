9bccf5a50799fc4fc10ae1de06763e0dd0ff0ebe
Ayaan <onestar7984@gmail.com>
2023-02-01 20:44:55 +0900

    feat: CoreDataRepositoryMock 구현

--- GyroDataTests/ServiceTests/Common/CoreDataRepositoryMock.swift
+++ GyroDataTests/ServiceTests/Common/CoreDataRepositoryMock.swift
@@ -2,7 +2,31 @@
 //  CoreDataRepositoryMock.swift
 //  GyroDataTests
 //
-//  Created by Ayaan on 2023/02/01.
+//  Created by Ayaan, Wonbi on 2023/02/01.
 //
 
-import Foundation
+@testable import GyroData
+
+final class CoreDataRepositoryMock: CoreDataRepository {
+    typealias Domain = GyroData.Motion
+    typealias Entity = GyroData.MotionMO
+    
+    var isCalledCreateFunction: Bool = false
+    var isCalledReadFunction: Bool = false
+    var isCalledDeleteFunction: Bool = false
+    let returnValuesOfReadFunction: [GyroData.MotionMO] = []
+    
+    func create(_ domain: GyroData.Motion, completion: @escaping (Result<Void, Error>) -> Void) {
+        isCalledCreateFunction = true
+        completion(.success(()))
+    }
+    
+    func read(from offset: Int) throws -> [GyroData.MotionMO] {
+        isCalledReadFunction = true
+        return returnValuesOfReadFunction
+    }
+    
+    func delete(with id: String) throws {
+        isCalledDeleteFunction = true
+    }
+}
