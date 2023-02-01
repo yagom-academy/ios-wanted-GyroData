b87647b65cf778a0d42fd5a179e7e649b63ed795
jpush <lj845854@gmail.com>
2023-02-01 15:40:29 +0900

    feat : accelerometer 측정 시작 메서드 구현
    - switch문을 위해 gyroscopeMeasureStart 메서드 또한 틀 구성
    - Timer를 프로퍼티로 가지고 measureStop이 실행되었을 때 타이머 stop

--- GyroData/Source/Domain/SensorMeasureService.swift
+++ GyroData/Source/Domain/SensorMeasureService.swift
@@ -2,10 +2,12 @@
 //  SensorMeasureService.swift
 //  GyroData
 //
-//  Created by 이정민 on 2023/02/01.
+//  Created by Kyo, JPush on 2023/02/01.
 //
 
+import CoreMotion
 import Foundation
+
 protocol MeasureDelegate: AnyObject {
     typealias Values = (x: Double, y: Double, z: Double)
     
@@ -13,4 +15,65 @@ protocol MeasureDelegate: AnyObject {
     func nonGyroscopeMeasurable()
     
     func updateData(_ data: [Values])
+}
+
+final class SensorMeasureService {
+    typealias Values = (x: Double, y: Double, z: Double)
+    
+    private var data: [Values] = [] {
+        didSet {
+            delegate?.updateData(data)
+        }
+    }
+    
+    private weak var delegate: MeasureDelegate?
+    private var timer: Timer = .init()
+    private let motionManager: CMMotionManager
+    
+    init(delegate: MeasureDelegate) {
+        self.delegate = delegate
+        self.motionManager = .init()
+    }
+    
+    func measureStart(_ sensorType: Sensor, interval: TimeInterval, duration: TimeInterval) {
+        data = .init()
+        
+        switch sensorType {
+        case .accelerometer:
+            accelerometerMeasureStart(interval, duration)
+        case .gyroscope:
+            gyroscopeMeasureStart(interval, duration)
+        }
+    }
+    
+    func measureStop() {
+        timer.invalidate()
+    }
+}
+
+private extension SensorMeasureService {
+    func accelerometerMeasureStart(_ interval: TimeInterval, _ duration: TimeInterval) {
+        guard motionManager.isAccelerometerAvailable else {
+            delegate?.nonAccelerometerMeasurable()
+            return
+        }
+        
+        motionManager.accelerometerUpdateInterval = interval
+        motionManager.startAccelerometerUpdates()
+        
+        timer = Timer(fire: Date(), interval: interval / duration, repeats: true) { timer in
+            if let data = self.motionManager.accelerometerData {
+                let accelerationData = (data.acceleration.x, data.acceleration.y, data.acceleration.z)
+                
+                self.data.append(accelerationData)
+            }
+        }
+        
+        RunLoop.current.add(timer, forMode: .common)
+    }
+    
+    
+    func gyroscopeMeasureStart(_ interval: TimeInterval, _ duration: TimeInterval) {
+        motionManager.gyroUpdateInterval = interval
+    }
 }
