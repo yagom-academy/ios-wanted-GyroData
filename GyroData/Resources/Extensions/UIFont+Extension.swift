2ba7b054243be11150011a57f8444754549672d5
Mangdi <lkw4670@naver.com>
2023-02-01 16:17:18 +0900

    feat: UIFont extention 파일생성 및 preferredFont메서드 작성

---
+++ GyroData/Resources/Extensions/UIFont+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont+Extension.swift
+//  GyroData
+//
+//  Created by Mangdi on 2023/02/01.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
+        let metrics = UIFontMetrics(forTextStyle: style)
+        let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+        let font = UIFont.systemFont(ofSize: desc.pointSize, weight: weight)
+        return metrics.scaledFont(for: font)
+    }
+}
