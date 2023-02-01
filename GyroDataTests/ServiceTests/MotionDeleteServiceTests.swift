d6b336a041e940feaa35b99c93559afd56504ea4
wonbi92 <bin9239@gmail.com>
2023-02-01 21:40:21 +0900

    test: MotionDeleteService 테스트 케이스 추가

--- GyroDataTests/ServiceTests/MotionDeleteServiceTests.swift
+++ GyroDataTests/ServiceTests/MotionDeleteServiceTests.swift
@@ -6,30 +6,38 @@
 //
 
 import XCTest
+@testable import GyroData
 
 final class MotionDeleteServiceTests: XCTestCase {
+    var coreDataRepository: CoreDataRepositoryMock!
+    var fileManagerRepository: FileManagerRepositoryMock!
+    var service: GyroData.MotionDeleteService<CoreDataRepositoryMock, FileManagerRepositoryMock>!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        coreDataRepository = CoreDataRepositoryMock()
+        fileManagerRepository = FileManagerRepositoryMock()
+        service = GyroData.MotionDeleteService(
+            coreDataRepository: coreDataRepository,
+            fileManagerRepository: fileManagerRepository
+        )
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        coreDataRepository = nil
+        fileManagerRepository = nil
+        service = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_Service의delete메서드를호출했을때_Repository의delete메서드들이호출되는지() {
+        // given
+        let _ = service.delete("")
+        
+        // when
+        let isCoreDataDeleteCalled = service.coreDataRepository.isCalledDeleteFunction
+        let isFileManagerDeleteCalled = service.fileManagerRepository.isCalledDeleteFunction
+        
+        // then
+        XCTAssertEqual(true, isCoreDataDeleteCalled)
+        XCTAssertEqual(true, isFileManagerDeleteCalled)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
