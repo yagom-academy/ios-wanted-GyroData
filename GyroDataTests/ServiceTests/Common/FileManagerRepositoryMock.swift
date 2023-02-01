57570c0ca799c2679256a1fd907fa9f9c27d8010
Ayaan <onestar7984@gmail.com>
2023-02-01 20:45:06 +0900

    feat: FileManagerRepositoryMock 구현

--- GyroDataTests/ServiceTests/Common/FileManagerRepositoryMock.swift
+++ GyroDataTests/ServiceTests/Common/FileManagerRepositoryMock.swift
@@ -2,7 +2,37 @@
 //  FileManagerRepositoryMock.swift
 //  GyroDataTests
 //
-//  Created by Ayaan on 2023/02/01.
+//  Created by Ayaan, Wonbi on 2023/02/01.
 //
 
-import Foundation
+@testable import GyroData
+
+final class FileManagerRepositoryMock: FileManagerRepository {
+    typealias Domain = GyroData.Motion
+    typealias Entity = GyroData.MotionDTO
+    
+    var isCalledCreateFunction: Bool = false
+    var isCalledReadFunction: Bool = false
+    var isCalledDeleteFunction: Bool = false
+    let returnValueOfReadFunction: GyroData.MotionDTO = .init(id: .init(),
+                                                              date: .init(),
+                                                              type: .init(),
+                                                              time: .init(),
+                                                              x: [],
+                                                              y: [],
+                                                              z: [])
+    
+    func create(_ domain: GyroData.Motion, completion: @escaping (Result<Void, Error>) -> Void) {
+        isCalledCreateFunction = true
+        completion(.success(()))
+    }
+    
+    func read(with id: String) throws -> GyroData.MotionDTO {
+        isCalledReadFunction = true
+        return returnValueOfReadFunction
+    }
+    
+    func delete(with id: String) throws {
+        isCalledDeleteFunction = true
+    }
+}
