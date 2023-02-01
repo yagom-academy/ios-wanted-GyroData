c3299431c2e834bf37a2ee66c188d8ca942c674a
lnhk98 <hozzang666@gmail.com>
2023-02-01 15:11:52 +0900

    feat: AppDelegate에 CoreData container 및 save메서드 구현

--- GyroData/Source/App/AppDelegate.swift
+++ GyroData/Source/App/AppDelegate.swift
@@ -5,13 +5,11 @@
 //  Created by kjs on 2022/09/16.
 //
 
+import CoreData
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -31,6 +29,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    //MARK: CoreData
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MotionDataModel")
+        
+        container.loadPersistentStores { storeDescription, error in
+            guard error == nil else { return }
+        }
+        
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print(error)
+            }
+        }
+    }
 }
-
