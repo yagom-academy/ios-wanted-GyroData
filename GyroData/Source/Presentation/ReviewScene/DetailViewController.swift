59d59db780a195740b4dff78fada0a83a78835ad
Kyo <hyosing92@gmail.com>
2023-02-02 19:59:44 +0900

    feat : DetailViewModel, DetailViewController 파일 생성

---
+++ GyroData/Source/Presentation/ReviewScene/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  GyroData
+//
+//  Created by parkhyo on 2023/02/02.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ GyroData/Source/Presentation/ReviewScene/DetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewModel.swift
+//  GyroData
+//
+//  Created by parkhyo on 2023/02/02.
+//
+
+import Foundation
+
+final class DetailViewModel {
+
+}
