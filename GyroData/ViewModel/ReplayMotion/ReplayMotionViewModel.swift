20a5f9f598410921924c7d0a49c0570278f0722a
hashswim <ss991117@naver.com>
2023-02-03 01:03:46 +0900

    feat: ReplayMotionViewModel 역할 정의

--- GyroData/ViewModel/ReplayMotion/ReplayMotionViewModel.swift
+++ GyroData/ViewModel/ReplayMotion/ReplayMotionViewModel.swift
@@ -4,3 +4,47 @@
 //
 //  Created by Aaron, Gundy on 2023/01/31.
 //
+
+import Foundation
+
+final class ReplayMotionViewModel {
+
+    private var fileManager: FileManager
+
+    private var motionDataListHandler: (() -> ())?
+
+    init() {
+        self.fileManager = FileManager.shared
+    }
+
+    @objc
+    private func motionDataBind() {
+        motionDataListHandler?()
+    }
+
+    func bindMotionDataList(handler: @escaping () -> ()) {
+        self.motionDataListHandler = handler
+    }
+
+    func fetchMotionDataList() -> [MotionData] {
+        return fileManager.fetchData()
+    }
+
+    func bindPlayButton() {
+
+    }
+
+    func bindStopButton() {
+
+    }
+
+    func bindPlayTime() {
+
+    }
+
+    func bindCellData() {
+        
+    }
+
+
+}
