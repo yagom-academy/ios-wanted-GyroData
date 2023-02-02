988df5a57268a7b50938a8d95d3023ec075c0372
wonbi92 <bin9239@gmail.com>
2023-02-02 12:24:10 +0900

    feat: MotionsListViewModel의 action 메서드 구현
    - 액션 메서드에 사용하는 로직 분리

--- GyroData/Scene/MotionsList/MotionsListViewModel.swift
+++ GyroData/Scene/MotionsList/MotionsListViewModel.swift
@@ -33,5 +33,52 @@ struct MotionsListViewModel {
     weak var delegate: MotionsListViewModelDelegate?
     
     mutating func action(_ action: Action) {
+        switch action {
+        case .viewWillApear:
+            setInitialMotions()
+        case .nextPageRequest:
+            setNextPageMotions()
+        case .motionTap(let indexPath):
+            tapMotionAction(indexPath: indexPath)
+        case .motionDelete(let indexPath):
+            tapMotionDeleteAction(indexPath: indexPath)
+        case .motionPlay(let indexPath):
+            tapMotionPlayAction(indexPath: indexPath)
+        }
+    }
+    
+    mutating private func setInitialMotions() {
+        guard let initialMotions = readService.read(from: 0) else { return }
+        
+        motions = initialMotions
+    }
+    
+    mutating private func setNextPageMotions() {
+        guard motions.count % 10 == 0 else { return }
+        
+        let currentPage: Int = Int(motions.count / 10)
+        guard let nextPageMotions = readService.read(from: currentPage * 10), !nextPageMotions.isEmpty else { return }
+        
+        motions += nextPageMotions
+    }
+    
+    private func tapMotionAction(indexPath: IndexPath) {
+        let selectedMotion = motions[indexPath.row]
+        
+        delegate?.motionsListViewModel(selectedGraphMotion: selectedMotion)
+    }
+    
+    mutating private func tapMotionDeleteAction(indexPath: IndexPath) {
+        let selectedMotion = motions[indexPath.row]
+        
+        if deleteService.delete(selectedMotion.id) {
+            motions.remove(at: indexPath.row)
+        }
+    }
+    
+    private func tapMotionPlayAction(indexPath: IndexPath) {
+        let selectedMotion = motions[indexPath.row]
+        
+        delegate?.motionsListViewModel(selectedPlayMotion: selectedMotion)
     }
 }
