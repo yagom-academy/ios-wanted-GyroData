4fb9cf40485ff8c1e90155f97c1004e5d4c4f075
lws2269 <lws2269@kakao.com>
2023-02-02 20:56:18 +0900

    feat: MesartViewType 열거형 타입 구현

---
+++ GyroData/Model/MesearViewType.swift
@@ -0,0 +1,13 @@
+//
+//  MesearViewType.swift
+//  GyroData
+//
+//  Created by leewonseok on 2023/02/02.
+//
+
+import Foundation
+
+enum MesearViewType: String{
+    case view = "View"
+    case play = "Play"
+}
