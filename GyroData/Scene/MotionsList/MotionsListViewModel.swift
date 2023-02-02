9e5aa9e521f084957317106c34399e0d4d781563
wonbi92 <bin9239@gmail.com>
2023-02-02 11:31:50 +0900

    feat: MotionsListViewModel 구조체, MotionsListViewModelDelegate 프로토콜, Action 열거체 구현

--- GyroData/Scene/MotionsList/MotionsListViewModel.swift
+++ GyroData/Scene/MotionsList/MotionsListViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+protocol MotionsListViewModelDelegate: AnyObject {
+    func motionsListViewModel(didChange motions: [Motion])
+    func motionsListViewModel(selectedGraphMotion motion: Motion)
+    func motionsListViewModel(selectedPlayMotion motion: Motion)
+}
+
+struct MotionsListViewModel {
+    enum Action {
+        case viewWillApear
+        case nextPageRequest
+        case motionTap(indexPath: IndexPath)
+        case motionDelete(indexPath: IndexPath)
+        case motionPlay(indexPath: IndexPath)
+    }
+    
+    var motions: [Motion] = [] {
+        didSet {
+            delegate?.motionsListViewModel(didChange: motions)
+        }
+    }
+    
+    let readService: CoreDataMotionReadable
+    let deleteService: MotionDeletable
+    weak var delegate: MotionsListViewModelDelegate?
+    
+    mutating func action(_ action: Action) {
+    }
+}
