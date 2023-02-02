747895fa59ad6a44a0f0b71db2d777f822ec975c
Kyo <hyosing92@gmail.com>
2023-02-02 21:56:10 +0900

    feat : DetailViewModel 구현

--- GyroData/Source/Presentation/ReviewScene/DetailViewModel.swift
+++ GyroData/Source/Presentation/ReviewScene/DetailViewModel.swift
@@ -2,11 +2,21 @@
 //  DetailViewModel.swift
 //  GyroData
 //
-//  Created by parkhyo on 2023/02/02.
+//  Created by Kyo, JPush on 2023/02/02.
 //
 
 import Foundation
 
 final class DetailViewModel {
+    private var measureData: MeasureData
+    
+    init(data: MeasureData) {
+        measureData = data
+    }
+}
 
+extension DetailViewModel {
+    func bindData(handler: @escaping (MeasureData) -> Void) {
+        handler(measureData)
+    }
 }
