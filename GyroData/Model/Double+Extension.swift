2bc746d1ad3481f6ed081d57d82f86dc512f3fbe
lj-7-77 <swtw72@gmail.com>
2023-02-02 21:54:42 +0900

    fix: Double+Extension 두 파일 하나로 병합

--- GyroData/Model/Double+Extension.swift
+++ GyroData/Model/Double+Extension.swift
@@ -2,12 +2,21 @@
 //  Double+Extension.swift
 //  GyroData
 //
-//  Created by 임지연 on 2023/02/02.
+//  Created by stone, LJ on 2023/02/02.
 //
 
 import Foundation
 
 extension Double {
+    func decimalPlace(_ decimalPoint: Int) -> Double {
+        var point: Double = 1
+        
+        for _ in 0...decimalPoint {
+            point *= 10
+        }
+        return floor(self * point) / point
+    }
+    
     func timeDecimal() -> Double? {
         return Double(String(format: "%.1f", self))
     }
