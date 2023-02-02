6c4fc9876a5d33dd2fac09a21cd3b6d05c7933c5
Ayaan <onestar7984@gmail.com>
2023-02-02 16:42:26 +0900

    feat: MotionMeasurementViewModelDelegate 구현

--- GyroData/Scene/MotionMeasure/MotionMeasureViewModel.swift
+++ GyroData/Scene/MotionMeasure/MotionMeasureViewModel.swift
@@ -5,4 +5,9 @@
 //  Created by Ayaan, Wonbi on 2023/01/31.
 //
 
-import Foundation
+protocol MotionMeasurementViewModelDelegate: AnyObject {
+    func motionMeasurementViewModel(measuredData data: MotionDataType, takenCurrentTime time: Double)
+    func motionMeasurementViewModel(isCompletedInMotionMeasurement: Bool)
+    func motionMeasurementViewModel(isSucceedInCreating: Bool)
+    func motionMeasurementViewModel(alertStyleToPresent: AlertStyle)
+}
