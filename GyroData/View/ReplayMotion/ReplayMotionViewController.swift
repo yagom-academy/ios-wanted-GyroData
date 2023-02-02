24154fd902c4ba7a4a9fe92e215037311ef9ecb1
hashswim <ss991117@naver.com>
2023-02-03 03:22:24 +0900

    feat: ReplayMotionViewController UI 기본 구현

--- GyroData/View/ReplayMotion/ReplayMotionViewController.swift
+++ GyroData/View/ReplayMotion/ReplayMotionViewController.swift
@@ -9,21 +9,102 @@ import UIKit
 
 class ReplayMotionViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private var replayMotionViewModel: ReplayMotionViewModel?
 
-        // Do any additional setup after loading the view.
+    init(replayMotionViewModel: ReplayMotionViewModel? = ReplayMotionViewModel()) {
+        self.replayMotionViewModel = replayMotionViewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+
+        return label
+    }()
+
+    let typeLabel: UILabel = {
+        let label = UILabel()
+
+        return label
+    }()
+
+    let graphView: UIView = {
+        let view = UIView()
+
+        return view
+    }()
+
+    let playButton: UIButton = {
+        let btn = UIButton()
+
+        return btn
+    }()
+
+    let stopButton: UIButton = {
+        let btn = UIButton()
+
+        return btn
+    }()
+
+    lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [playButton, stopButton])
+        stackView.axis = .horizontal
+
+        return stackView
+    }()
+
+    lazy var containerStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [dateLabel, typeLabel, graphView, buttonStackView])
+        stackView.spacing = 8
+        return stackView
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        configureUI()
+    }
+
+    // MARK: - setUp UI
+    func configureUI() {
+        configureHierarchy()
+        configureLayout()
+        configureNavigation()
+    }
+
+    func configureHierarchy() {
+        view.addSubview(containerStackView)
+    }
+
+    func configureLayout() {
+        containerStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            dateLabel.heightAnchor.constraint(equalToConstant: 16),
+
+            typeLabel.heightAnchor.constraint(equalToConstant: 36),
+
+            graphView.widthAnchor.constraint(equalTo: containerStackView.widthAnchor),
+            graphView.heightAnchor.constraint(equalTo: graphView.widthAnchor),
+
+            containerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            containerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 16),
+            containerStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    func configureNavigation() {
+        self.navigationController?.title = "다시 보기"
+    }
+
+    // MARK: - binding Data
+
+
 
 }
