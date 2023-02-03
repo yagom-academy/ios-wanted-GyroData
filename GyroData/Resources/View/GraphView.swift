5c68d2d7577c9ad272242e681492576a444110da
Mangdi <lkw4670@naver.com>
2023-02-03 15:52:12 +0900

    feat: GraphView 파일 생성

---
+++ GyroData/Resources/View/GraphView.swift
@@ -0,0 +1,24 @@
+//
+//  GraphView.swift
+//  GyroData
+//
+//  Created by Mangdi on 2023/02/03.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+    }
+
+}
