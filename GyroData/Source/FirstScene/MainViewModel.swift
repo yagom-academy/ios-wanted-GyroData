b282f24e64c3e781f1330cb2adecaf2918091cbe
lnhk98 <hozzang666@gmail.com>
2023-02-04 14:23:34 +0900

    feat: MainViewModel에 CoreDataManageable 프로토콜 채택 및 로직 추가

--- GyroData/Source/FirstScene/MainViewModel.swift
+++ GyroData/Source/FirstScene/MainViewModel.swift
@@ -7,15 +7,73 @@
 
 import Foundation
 
-final class MainViewModel {
-    private(set) var motionDatas: [MotionData] = [] {
+final class MainViewModel: CoreDataManageable {
+    private var motionDatas: [MotionData] = [] {
         didSet {
             self.dataHandler?(motionDatas)
         }
     }
     private var dataHandler: (([MotionData]) -> Void)?
+    private(set) var hasNext: Bool = true
+    private var coreDataOffset: Int = 0 {
+        didSet {
+            fetchDatas()
+        }
+    }
     
     func bindData(_ handler: @escaping (([MotionData]) -> Void)) {
         dataHandler = handler
+    }
+    
+    func saveMotionData(_ data: MotionData) {
+        do {
+            try saveCoreData(motionData: data)
+            fetchDatas()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func fetchDatas() {
+        let fetchResult = readCoreData(offset: coreDataOffset)
+        
+        switch fetchResult {
+        case .success(let motionEntities):
+            if motionEntities.count == 0 {
+                hasNext = false
+                return
+            } else {
+                hasNext = true
+            }
+            
+            let convertedDatas = motionEntities.compactMap { convertEntityToData($0) }
+            
+            motionDatas += convertedDatas
+        case .failure(let error):
+            print(error)
+        }
+    }
+    
+    func increaseOffset() {
+        coreDataOffset += 10
+    }
+    
+    private func convertEntityToData(_ entity: MotionEntity) -> MotionData? {
+        guard let date = entity.measuredDate,
+              let typeString = entity.type,
+              let type = MotionType(rawValue: typeString),
+              let id = entity.id
+        else {
+            return nil
+        }
+        
+        let motionData = MotionData(
+            measuredDate: date,
+            duration: entity.duration,
+            type: type,
+            id: id
+        )
+        
+        return motionData
     }
 }
