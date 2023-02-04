9c4ae2d0c6b2bfb67ebde5a43de69f12669a3d0e
jonghancha <jonghancha@gmail.com>
2023-02-04 20:22:52 +0900

    feat: setupNavigationBar, bind 메서드 추가

--- GyroData/Source/View/DetailView/DetailViewController.swift
+++ GyroData/Source/View/DetailView/DetailViewController.swift
@@ -7,50 +7,34 @@
 
 import UIKit
 
-enum PageType: String {
-    case play = "Play"
-    case view = "View"
+extension DateFormatter {
+    static let measuredDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        
+        return dateFormatter
+    }()
+}
+
+enum PageType {
+    case play
+    case view
+    
+    var description: String {
+        switch self {
+        case .play:
+            return "Play"
+        case .view:
+            return "View"
+        }
+    }
 }
 
 final class DetailViewController: UIViewController {
+    private let detailView = DetailView()
     private let viewModel: DetailViewModel
     
-    private let stackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .leading
-        stackView.distribution = .fill
-        
-        return stackView
-    }()
-    
-    private let dateLabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    private let pageTypeLabel = {
-        let label = UILabel()
-        label.font = .preferredFont(forTextStyle: .body, compatibleWith: nil)
-        
-        return label
-    }()
-    
-    private var graphView = UIView()
-    
-    private let playButton = {
-        let button = UIButton()
-        
-        return button
-    }()
-    
-    private let timerLabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-
     init(viewModel: DetailViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -60,50 +44,61 @@ final class DetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func loadView() {
+        view = detailView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        addSubViews()
-        // Do any additional setup after loading the view.
-    }
-    
-    private func addSubViews() {
-        view.addSubview(stackView)
-        stackView.addArrangedSubview(dateLabel)
-        stackView.addArrangedSubview(pageTypeLabel)
-        stackView.addArrangedSubview(graphView)
-        stackView.addArrangedSubview(playButton)
-        stackView.addArrangedSubview(timerLabel)
-        setGraph()
-    }
-    
-    private func setGraph() {
-        let padding: CGFloat = 100
         
-        let frame = CGRect(
-            x: 0,
-            y: 0,
-            width: self.view.frame.width - padding,
-            height: self.view.frame.height - padding
-        )
-        let view = LineGraphView( // 그래프 뷰
-            frame: frame,
-            values: [20, 10, 30, 20, 50, 100, 10, 10]
-        )
-
-        view.center = CGPoint(x: self.view.frame.size.width / 2, y: self.view.frame.size.height / 2)
-        self.view.addSubview(view)
-        graphView = view
+        bind()
+        setupView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupView() {
+        setupNavigationBar()
+        viewModel.convertModelData()
     }
-    */
+    
+    private func setupNavigationBar() {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = .white
+        navigationBarAppearance.shadowColor = .clear
+        navigationItem.title = "다시보기"
 
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+    }
+    
+    private func bind() {
+        viewModel.bindDate { [weak self] dateString in
+            self?.detailView.dateLabel.text = dateString
+        }
+        viewModel.bindPageType { [weak self] pageTypeString in
+            self?.detailView.pageTypeLabel.text = pageTypeString
+        }
+        viewModel.bindGraphData { [weak self] axisX, axisY, axisZ in
+            
+        }
+    }
+    
+    
+    
+//    private func setGraph() {
+//        let padding: CGFloat = 100
+//
+//        let frame = CGRect(
+//            x: 0,
+//            y: 0,
+//            width: self.view.frame.width - padding,
+//            height: self.view.frame.height - padding
+//        )
+//        let view = LineGraphView( // 그래프 뷰
+//            frame: frame,
+//            values: [20, 10, 30, 20, 50, 100, 10, 10]
+//        )
+//
+//        view.center = CGPoint(x: self.view.frame.size.width / 2, y: self.view.frame.size.height / 2)
+//        self.view.addSubview(view)
+//        graphView = view
+//    }
 }
