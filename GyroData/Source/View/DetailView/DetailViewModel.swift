928f71fe989ce51dbf1752293ed8fb61d2218850
jonghancha <jonghancha@gmail.com>
2023-02-04 20:23:34 +0900

    feat: DetailViewModel 속성, bind, 클로저 추가

--- GyroData/Source/View/DetailView/DetailViewModel.swift
+++ GyroData/Source/View/DetailView/DetailViewModel.swift
@@ -5,16 +5,50 @@
 //  Created by Aejong on 2023/02/03.
 //
 
+import Foundation
+
 final class DetailViewModel {
-    private let model: MotionData
-    private let pageType: PageType
+    typealias StringHandler = (String) -> Void
+    typealias MotionMeasuresHandler = ([CGFloat], [CGFloat], [CGFloat]) -> Void
     
-    private let title: String = "다시보기"
+    private let model: MotionData
+    private let currentType: PageType
+    
+    private var date: String = String() {
+        didSet {
+            dateHandler?(date)
+        }
+    }
+    private var pageType: String = String() {
+        didSet {
+            pageTypeHandler?(pageType)
+        }
+    }
+    
+    private var dateHandler: StringHandler?
+    private var pageTypeHandler: StringHandler?
+    private var graphDataHandler: MotionMeasuresHandler?
     
     init(_ motionData: MotionData, by type: PageType) {
         model = motionData
-        pageType = type
+        currentType = type
     }
     
+    func convertModelData() {
+        date = DateFormatter.measuredDateFormatter.string(from: model.measuredDate)
+        pageType = currentType.description
+    }
     
+    func bindDate(_ handler: @escaping StringHandler) {
+        dateHandler = handler
+    }
+    
+    func bindPageType(_ handler: @escaping StringHandler) {
+        pageTypeHandler = handler
+    }
+    
+    func bindGraphData(_ handler: @escaping MotionMeasuresHandler) {
+        
+    }
 }
+
