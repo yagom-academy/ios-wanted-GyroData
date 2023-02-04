4bb131636678a295db7f1632e89fa7d7b00a739b
jonghancha <jonghancha@gmail.com>
2023-02-05 02:51:44 +0900

    feat: fetchMeasures 메서드 추가

--- GyroData/Source/View/DetailView/DetailViewModel.swift
+++ GyroData/Source/View/DetailView/DetailViewModel.swift
@@ -9,16 +9,24 @@ import Foundation
 
 final class DetailViewModel {
     typealias StringHandler = (String) -> Void
-    typealias MotionMeasuresHandler = ([CGFloat], [CGFloat], [CGFloat]) -> Void
+    typealias MotionMeasuresHandler = (MotionMeasures, Double) -> Void
     
     private let model: MotionData
     private let currentType: PageType
+    private let fileManager: FileManagerProtocol
+    
+    private var motionMeasures: MotionMeasures = MotionMeasures(axisX: [], axisY: [], axisZ: []) {
+        didSet {
+            measuresHandler?(motionMeasures, model.duration)
+        }
+    }
     
     private var date: String = String() {
         didSet {
             dateHandler?(date)
         }
     }
+    
     private var pageType: String = String() {
         didSet {
             pageTypeHandler?(pageType)
@@ -27,16 +35,29 @@ final class DetailViewModel {
     
     private var dateHandler: StringHandler?
     private var pageTypeHandler: StringHandler?
-    private var graphDataHandler: MotionMeasuresHandler?
+    private var measuresHandler: MotionMeasuresHandler?
     
-    init(_ motionData: MotionData, by type: PageType) {
+    init(_ motionData: MotionData, by type: PageType, fileManager: FileManagerProtocol) {
         model = motionData
         currentType = type
+        self.fileManager = fileManager
     }
     
-    func convertModelData() {
+    func fetchData() {
         date = DateFormatter.measuredDateFormatter.string(from: model.measuredDate)
         pageType = currentType.description
+        fetchMeasures()
+    }
+    
+    private func fetchMeasures() {
+        fileManager.load(fileName: model.id) { result in
+            switch result {
+            case .success(let data):
+                self.motionMeasures = data
+            case .failure(let failure):
+                print(failure)
+            }
+        }
     }
     
     func bindDate(_ handler: @escaping StringHandler) {
@@ -48,7 +69,7 @@ final class DetailViewModel {
     }
     
     func bindGraphData(_ handler: @escaping MotionMeasuresHandler) {
-        
+        measuresHandler = handler
     }
 }
 
