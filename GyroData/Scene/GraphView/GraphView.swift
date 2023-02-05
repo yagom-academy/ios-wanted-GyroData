c8b36a17049ce3acd67fc0e690ab9362cd517950
wonbi92 <bin9239@gmail.com>
2023-02-05 11:05:20 +0900

    feat: x, y, z를 나타내는 Label 추가

--- GyroData/Scene/GraphView/GraphView.swift
+++ GyroData/Scene/GraphView/GraphView.swift
@@ -8,6 +8,44 @@
 import UIKit
 
 final class GraphView: UIView {
+    static let segmentColor: [UIColor] = [.systemRed, .systemGreen, .systemBlue]
+    
+    private let xLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption2)
+        label.textAlignment = .center
+        label.textColor = segmentColor[0]
+        label.text  = "x: 123123"
+        return label
+    }()
+    
+    private let yLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption2)
+        label.textAlignment = .center
+        label.textColor = segmentColor[1]
+        label.text  = "x: 123123"
+        return label
+    }()
+    
+    private let zLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption2)
+        label.textAlignment = .center
+        label.textColor = segmentColor[2]
+        label.text  = "x: 123123"
+        return label
+    }()
+    
+    private let labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     private var gridHeight: CGFloat {
         return bounds.height / 8
     }
@@ -37,9 +75,27 @@ final class GraphView: UIView {
         layer.borderWidth = 1
         layer.borderColor = UIColor.systemGray4.cgColor
         backgroundColor = .systemBackground
+        
+        configureView()
+        configureLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        [xLabel, yLabel, zLabel].forEach(labelStackView.addArrangedSubview(_:))
+        
+        addSubview(labelStackView)
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            labelStackView.topAnchor.constraint(equalTo: topAnchor),
+            labelStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            labelStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            labelStackView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.125)
+        ])
     }
 }
