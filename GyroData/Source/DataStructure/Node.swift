bfeb90a33f00e8ce065eb2f7ed9bdf11240431ac
kkuldak1@naver.com <kkuldak1@naver.com>
2023-06-12 20:16:36 +0900

    feat: 노드의 개수 제한이 있는 자료구조 Queue 타입 구현

--- GyroData/Source/DataStructure/Node.swift
+++ GyroData/Source/DataStructure/Node.swift
@@ -5,13 +5,11 @@
 //  Created by kokkilE on 2023/06/12.
 //
 
-import Foundation
-
 final class Node<Element> {
-    private(set) var data: Element
+    let data: Element
     var next: Node<Element>?
     
-    init(data: Element, next: Node<Element>? = nil) {
+    init(_ data: Element, next: Node<Element>? = nil) {
         self.data = data
         self.next = next
     }
