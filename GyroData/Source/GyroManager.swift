53b2c37cdc9eea446b0778746c48f67fced1f126
kkuldak1@naver.com <kkuldak1@naver.com>
2023-06-12 21:10:43 +0900

    feat: 자이로 측정을 시작하는 GyroManager의 start 기능 구현

--- GyroData/Source/GyroManager.swift
+++ GyroData/Source/GyroManager.swift
@@ -13,36 +13,55 @@ final class GyroManager {
     }
     
     static let shared = GyroManager()
-    
     private let motionManager = CMMotionManager()
+    private var dataType: GyroData.DataType?
+    private var gyroData: GyroData?
     
     private init() {
         setupPeriod()
     }
     
-    func startAccelerometerUpdates() {
+    func start(dataType: GyroData.DataType) {
+        self.dataType = dataType
+        gyroData = GyroData(dataType: dataType)
+        
+        switch dataType {
+        case .accelerometer:
+            startAccelerometerUpdates()
+        case .gyro:
+            startGyroUpdates()
+        }
+    }
+    
+    private func startAccelerometerUpdates() {
         motionManager.accelerometerUpdateInterval = 0.1
         
-        motionManager.startAccelerometerUpdates(to: .current!) { data, error in
+        motionManager.startAccelerometerUpdates(to: .current!) { [weak self] data, error in
             guard let x = data?.acceleration.x,
                   let y = data?.acceleration.y,
                   let z = data?.acceleration.z else {
                 // error handle
                 return
             }
+            
+            let data = Coordinate(x: x, y: y, z: z)
+            self?.gyroData?.add(data)
         }
     }
     
-    func startGyroUpdates() {
+    private func startGyroUpdates() {
         motionManager.gyroUpdateInterval = 0.1
         
-        motionManager.startGyroUpdates(to: .current!) { data, error in
+        motionManager.startGyroUpdates(to: .current!) { [weak self] data, error in
             guard let x = data?.rotationRate.x,
                   let y = data?.rotationRate.y,
                   let z = data?.rotationRate.z else {
                 // error handle
                 return
             }
+            
+            let data = Coordinate(x: x, y: y, z: z)
+            self?.gyroData?.add(data)
         }
     }
     
--- GyroData/Source/Model/GyroData.swift
+++ GyroData/Source/Model/GyroData.swift
@@ -5,14 +5,32 @@
 //  Created by kokkilE on 2023/06/12.
 //
 
+import Foundation
+
 final class GyroData {
     enum DataType {
         case accelerometer
         case gyro
+        
+        var description: String {
+            switch self {
+            case .accelerometer:
+                return "Accelerometer"
+            case .gyro:
+                return "Gyro"
+            }
+        }
     }
     
     private let dataType: DataType
     private var queue = LimitedQueue<Coordinate>()
+    private var date: Date?
+    private var duration: Double? {
+        let duration = Double(queue.count) / 10.0
+        let formattedDuration = String(format: "%.1f", duration)
+        
+        return Double(formattedDuration)
+    }
     
     init(dataType: DataType) {
         self.dataType = dataType
@@ -20,5 +38,6 @@ final class GyroData {
     
     func add(_ data: Coordinate) {
         queue.enqueue(data)
+        date = Date()
     }
 }
