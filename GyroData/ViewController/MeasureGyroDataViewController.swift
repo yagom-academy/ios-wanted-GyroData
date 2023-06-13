dd09142844db8bcbb46d502664e7d458f7227084
yijiye <jiyee4013@naver.com>
2023-06-13 12:25:30 +0900

    feat: MeasureGyroDataVC UI 기본 틀 구현

--- GyroData/ViewController/MeasureGyroDataViewController.swift
+++ GyroData/ViewController/MeasureGyroDataViewController.swift
@@ -15,6 +15,45 @@ final class MeasureGyroDataViewController: UIViewController {
         
         return control
     }()
+    
+    private let graphView: UIView = {
+        let view = UIView()
+        let lineWidth: CGFloat = 3
+        view.layer.borderWidth = lineWidth
+        
+        return view
+    }()
+    
+    private let labelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .leading
+        
+        return stackView
+    }()
+    
+    private let measurementButton: UIButton = {
+        let button = UIButton()
+        let title = "측정"
+        let fontSize: CGFloat = 25
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: fontSize)
+        
+        return button
+    }()
+    
+    private let stopButton: UIButton = {
+        let button = UIButton()
+        let title = "정지"
+        let fontSize: CGFloat = 25
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: fontSize)
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +65,7 @@ final class MeasureGyroDataViewController: UIViewController {
         setUpNavigationBar()
         
         view.backgroundColor = .white
-        view.addSubview(segmentedControl)
+        setUpUI()
         setUpSegmentedControl()
     }
     
@@ -47,28 +86,55 @@ final class MeasureGyroDataViewController: UIViewController {
     }
     
     private func setUpSegmentedControl() {
+        let fontSize: CGFloat = 20
         segmentedControl.selectedSegmentIndex = 0
         segmentedControl.selectedSegmentTintColor = .systemTeal
         
         let normalAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.boldSystemFont(ofSize: 20),
+            .font: UIFont.boldSystemFont(ofSize: fontSize),
             .foregroundColor: UIColor.darkGray
             ]
         let selectedAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.boldSystemFont(ofSize: 20),
+            .font: UIFont.boldSystemFont(ofSize: fontSize),
             .foregroundColor: UIColor.white
             ]
         
         segmentedControl.setTitleTextAttributes(normalAttributes, for: .normal)
         segmentedControl.setTitleTextAttributes(selectedAttributes, for: .selected)
         segmentedControl.addTarget(self, action: #selector(changeSensorType), for: .valueChanged)
-        
+    }
+    
+    private func setUpUI() {
+        view.addSubview(segmentedControl)
+        view.addSubview(graphView)
+        view.addSubview(labelStackView)
+        labelStackView.addArrangedSubview(measurementButton)
+        labelStackView.addArrangedSubview(stopButton)
+
         let safeArea = view.safeAreaLayoutGuide
+        let segmentControlTop: CGFloat = 10
+        let leading: CGFloat = 30
+        let trailing: CGFloat = -30
+        let contentsTop: CGFloat = 20
+        let bottom: CGFloat = -140
+        
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        graphView.translatesAutoresizingMaskIntoConstraints = false
+        labelStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            segmentedControl.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
-            segmentedControl.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 30),
-            segmentedControl.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -30)
+            segmentedControl.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: segmentControlTop),
+            segmentedControl.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: leading),
+            segmentedControl.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: trailing),
+            
+            graphView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: contentsTop),
+            graphView.leadingAnchor.constraint(equalTo: segmentedControl.leadingAnchor),
+            graphView.trailingAnchor.constraint(equalTo: segmentedControl.trailingAnchor),
+            graphView.widthAnchor.constraint(equalTo: graphView.heightAnchor),
+            
+            labelStackView.topAnchor.constraint(equalTo: graphView.bottomAnchor, constant: contentsTop),
+            labelStackView.leadingAnchor.constraint(equalTo: segmentedControl.leadingAnchor),
+            labelStackView.trailingAnchor.constraint(equalTo: segmentedControl.trailingAnchor),
+            labelStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: bottom)
         ])
     }
     
